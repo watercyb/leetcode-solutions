8ac31a1e02eb16149cfae7912c27666fd461e1a4
watercyb <watercyb@hotmail.com>
2026-03-25 14:33:55 -0700

    Add solution: 116. Populating Next Right Pointers in Each Node

---
+++ 116. Populating Next Right Pointers in Each Node/116. Populating Next Right Pointers in Each Node.java
@@ -0,0 +1,46 @@
+/*
+ * Problem: 116. Populating Next Right Pointers in Each Node
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+ * Language: java
+ * Date: 2026-03-25
+ */
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+
+    public Node connect(Node root) {
+        if (root==null) return root;
+        if (root.left != null) {
+            root.left.next = root.right;
+            if (root.next != null)
+                root.right.next = root.next.left;
+            connect(root.left);
+            connect(root.right);
+        }
+        return root;
+    }
+}
