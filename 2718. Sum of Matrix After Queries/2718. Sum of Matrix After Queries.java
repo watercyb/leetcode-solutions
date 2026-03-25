c307e127db9bbe9b9ce9c415e3330de08fab5d1c
watercyb <watercyb@hotmail.com>
2026-03-25 10:17:46 -0700

    Add solution: 2718. Sum of Matrix After Queries

---
+++ 2718. Sum of Matrix After Queries/2718. Sum of Matrix After Queries.java
@@ -0,0 +1,33 @@
+/*
+ * Problem: 2718. Sum of Matrix After Queries
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/sum-of-matrix-after-queries/?envType=problem-list-v2&envId=hash-table
+ * Language: java
+ * Date: 2026-03-25
+ */
+
+class Solution {
+    public long matrixSumQueries(int n, int[][] queries) {
+        int row = n;
+        int col = n;
+        boolean[] rows = new boolean[n];
+        boolean[] cols = new boolean[n];
+        long res = 0;
+        for (int i = queries.length - 1; i >= 0; i--) {
+            if (queries[i][0] == 0) {
+                if (!rows[queries[i][1]]) {
+                    rows[queries[i][1]] = true;
+                    res += (long) row * queries[i][2];
+                    col--;
+                }
+            } else {
+                if (!cols[queries[i][1]]) {
+                    cols[queries[i][1]] = true;
+                    res += (long) col * queries[i][2];
+                    row--;
+                }
+            }
+        }
+        return res;
+    }
+}
