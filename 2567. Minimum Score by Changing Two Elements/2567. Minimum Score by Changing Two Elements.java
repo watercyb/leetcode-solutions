e79d10193f0145022d1cf06dcfdf3e66634672f7
watercyb <watercyb@hotmail.com>
2026-03-25 14:26:29 -0700

    Add solution: 2567. Minimum Score by Changing Two Elements

---
+++ 2567. Minimum Score by Changing Two Elements/2567. Minimum Score by Changing Two Elements.java
@@ -0,0 +1,14 @@
+/*
+ * Problem: 2567. Minimum Score by Changing Two Elements
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/minimum-score-by-changing-two-elements/
+ * Language: java
+ * Date: 2026-03-25
+ */
+
+class Solution {
+    public int minimizeSum(int[] nums) {
+        Arrays.sort(nums);
+        return Math.min(nums[nums.length-2]-nums[1], Math.min(nums[nums.length-1]-nums[2], nums[nums.length-3]-nums[0]));
+    }
+}
