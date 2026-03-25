e9875f6348c8ab3a3207f2722642624b8ff7344f
watercyb <watercyb@hotmail.com>
2026-03-25 10:04:53 -0700

    Add solution: 2495. Number of Subarrays Having Even Product

---
+++ 2495. Number of Subarrays Having Even Product/2495. Number of Subarrays Having Even Product.py
@@ -0,0 +1,21 @@
+#
+# Problem: 2495. Number of Subarrays Having Even Product
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/number-of-subarrays-having-even-product/?envType=problem-list-v2&envId=math
+# Language: python3
+# Date: 2026-03-25
+
+
+class Solution:
+    def evenProduct(self, nums: List[int]) -> int:
+        length = 0
+        res = (len(nums) + 1) * len(nums) // 2
+        for num in nums:
+            if (num & 1) == 0:
+                res -= (length + 1) * length // 2
+                length = 0
+            else:
+                length += 1
+        res -= (length + 1) * length // 2
+        return res
+
