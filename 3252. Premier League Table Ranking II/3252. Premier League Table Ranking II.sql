53d5561576df13527b65bfbb10a8dec57e2024af
watercyb <watercyb@hotmail.com>
2026-03-25 10:05:13 -0700

    Add solution: 3252. Premier League Table Ranking II

---
+++ 3252. Premier League Table Ranking II/3252. Premier League Table Ranking II.sql
@@ -0,0 +1,16 @@
+/*
+ * Problem: 3252. Premier League Table Ranking II
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/premier-league-table-ranking-ii/?envType=problem-list-v2&envId=ne414q67
+ * Language: mysql
+ * Date: 2026-03-25
+ */
+
+# Write your MySQL query statement below
+with r as (
+    select team_name, wins*3+draws as points, RANK() over (order by wins*3+draws desc) as position
+    from TeamStats
+)
+select *, CONCAT('Tier ', FLOOR((position-1)/(select COUNT(*) from TeamStats)/0.33)+1) as tier
+from r
+order by points desc, team_name
