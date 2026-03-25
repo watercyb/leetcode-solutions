8452ada74dfbc1053688d4df677f9915b88a0b7e
watercyb <watercyb@hotmail.com>
2026-03-24 23:10:32 -0700

    Add solution: 48. Rotate Image

---
+++ 48. Rotate Image/48. Rotate Image.java
@@ -0,0 +1,24 @@
+/*
+ * Problem: 48. Rotate Image
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/rotate-image/
+ * Language: java
+ * Date: 2026-03-25
+ */
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int l = matrix.length - 1;
+        for (int i = 0; i <= matrix.length / 2; i++) {
+            for (int j = i; j < matrix[0].length - 1 - i; j++) {
+                int[][] points = { { i, j }, { j, l - i },
+                        { l - i, l - j }, { l - j, i } };
+                int tmp = matrix[points[0][0]][points[0][1]];
+                matrix[points[0][0]][points[0][1]] = matrix[points[3][0]][points[3][1]];
+                matrix[points[3][0]][points[3][1]] = matrix[points[2][0]][points[2][1]];
+                matrix[points[2][0]][points[2][1]] = matrix[points[1][0]][points[1][1]];
+                matrix[points[1][0]][points[1][1]] = tmp;
+            }
+        }
+    }
+}
