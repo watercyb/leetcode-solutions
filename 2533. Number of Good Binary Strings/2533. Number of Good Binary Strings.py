c263ad2f7b0707202f36b2c9d47c89eec30df6be
watercyb <watercyb@hotmail.com>
2026-03-25 14:22:44 -0700

    Add solution: 2533. Number of Good Binary Strings

---
+++ 2533. Number of Good Binary Strings/2533. Number of Good Binary Strings.py
@@ -0,0 +1,20 @@
+#
+# Problem: 2533. Number of Good Binary Strings
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/number-of-good-binary-strings/
+# Language: python3
+# Date: 2026-03-25
+
+
+class Solution:
+    def goodBinaryStrings(
+        self, minLength: int, maxLength: int, oneGroup: int, zeroGroup: int
+    ) -> int:
+        mod = 1_000_000_007
+        dp = [0] * (maxLength + max(oneGroup, zeroGroup) + 1)
+        dp[0] = 1
+        for i in range(maxLength):
+            dp[i + oneGroup] = (dp[i + oneGroup] + dp[i]) % mod
+            dp[i + zeroGroup] = (dp[i + zeroGroup] + dp[i]) % mod
+        return sum(dp[minLength : maxLength + 1]) % mod
+
