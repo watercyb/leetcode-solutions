d2f293e1837d87e54d8d2e6eb521986c4ea50b87
watercyb <watercyb@hotmail.com>
2026-03-26 14:03:16 -0700

    Add solution: 2746. Decremental String Concatenation

---
+++ 2746. Decremental String Concatenation/2746. Decremental String Concatenation.rs
@@ -0,0 +1,46 @@
+/*
+ * Problem: 2746. Decremental String Concatenation
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/decremental-string-concatenation/?envType=problem-list-v2&envId=dynamic-programming
+ * Language: rust
+ * Date: 2026-03-26
+ */
+
+impl Solution {
+    pub fn minimize_concatenated_length(words: Vec<String>) -> i32 {
+        let mut dp: [[usize; 26]; 26] = [[100000000; 26]; 26];
+        let (l, r)=Self::getPair(&words[0]);
+        dp[l][r]=words[0].len();
+        for i in 1..words.len() {
+            let mut dp_next: [[usize; 26]; 26] = [[100000000; 26]; 26];
+            let (l, r)=Self::getPair(&words[i]);
+            let length=words[i].len();
+            for j in 0..26 {
+                for k in 0..26 {
+                    if k==l {
+                        dp_next[j][r]=dp_next[j][r].min(dp[j][k]+length-1);
+                    } else {
+                        dp_next[j][r]=dp_next[j][r].min(dp[j][k]+length);
+                    }
+                    if j==r {
+                        dp_next[l][k]=dp_next[l][k].min(dp[j][k]+length-1);
+                    } else {
+                        dp_next[l][k]=dp_next[l][k].min(dp[j][k]+length);
+                    }
+                }
+            }
+            dp=dp_next;
+        }
+        let mut res=100000000;
+        for i in 0..26 {
+            for j in 0..26 {
+                res=res.min(dp[i][j]);
+            }
+        }
+        return res as i32;
+    }
+
+    pub fn getPair(word: &String) -> (usize, usize) {
+        return ((word.as_bytes()[0] - b'a') as usize, (word.as_bytes()[word.len()-1] - b'a') as usize);
+    }
+}
