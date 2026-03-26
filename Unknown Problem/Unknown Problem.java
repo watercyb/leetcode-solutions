4ce1ddb32a22adf97b80ab35bad859880115a7c4
watercyb <watercyb@hotmail.com>
2026-03-25 22:46:58 -0700

    Update solution: Unknown Problem

--- Unknown Problem/Unknown Problem.java
+++ Unknown Problem/Unknown Problem.java
@@ -1,107 +1,75 @@
 /*
  * Problem: Unknown Problem
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/lexicographically-smallest-string-after-reverse-ii/
+ * Link: https://leetcode.com/problems/maximum-distinct-path-sum-in-a-binary-tree/
  * Language: java
- * Date: 2026-03-25
+ * Date: 2026-03-26
  */
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public String lexSmallest(String s) {
-        char[] chrs = s.toCharArray();
-        prefixes = new int[chrs.length];
-        suffixes = new int[chrs.length];
-        suffixe_revs = new int[chrs.length];
-        bases = new int[chrs.length];
-        long h = 0;
-        long h_rev = 0;
-        long base = 1;
-        for (int i = 0; i < chrs.length; i++) {
-            bases[i] = (int) base;
-            h = (h * 26 + chrs[i] - 'a') % mod;
-            prefixes[i] = (int) h;
-            base = base * 26 % mod;
-        }
-        h = 0;
-        for (int i = chrs.length - 1; i >= 0; i--) {
-            h = (h * 26 + chrs[i] - 'a') % mod;
-            h_rev = (h_rev + (chrs[i] - 'a') * bases[chrs.length - 1 - i]) % mod;
-            suffixes[i] = (int) h;
-            suffixe_revs[i] = (int) h_rev;
-        }
-        int max_type = 0;
-        int max_idx = chrs.length - 1;
-        for (int i = 0; i < chrs.length - 1; i++) {
-            if (compare(chrs, max_type, max_idx, 0, i)) {
-                max_idx = i;
+    public int maxSum(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+        dfsLinks(root, -1, lists, values);
+        int[][] links = new int[lists.size()][];
+        int[] vals = new int[values.size()];
+        for (int i = 0; i < links.length; i++) {
+            List<Integer> list = lists.get(i);
+            links[i] = new int[list.size()];
+            for (int j = 0; j < links[i].length; j++) {
+                links[i][j] = list.get(j);
             }
+            vals[i] = values.get(i);
         }
-        for (int i = chrs.length - 2; i >= 1; i--) {
-            if (compare(chrs, max_type, max_idx, 1, i)) {
-                max_type = 1;
-                max_idx = i;
-            }
+        int res = -100000000;
+        boolean[] seens = new boolean[2001];
+        for (int i = 0; i < links.length; i++) {
+            res = Math.max(res, dfs(links, vals, seens, i, -1));
         }
-        if (max_type == 0) {
-            return new StringBuilder(s.substring(0, max_idx))
-                    .append(new StringBuilder(s.substring(max_idx)).reverse().toString()).toString();
-        } else {
-            return new StringBuilder(s.substring(0, max_idx + 1)).reverse()
-                    .append(s.substring(max_idx + 1)).toString();
-        }
+        return res;
     }
 
-    int mod = 1_000_000_007;
-    int[] prefixes;
-    int[] suffixes;
-    int[] suffixe_revs;
-    int[] bases;
-
-    public int getH(int type, int i, int len) {
-        if (type == 0) {
-            if (len <= i)
-                return prefixes[len - 1];
-            long res = i == 0 ? 0 : ((long) prefixes[i - 1] * bases[len - i] % mod + mod) % mod;
-            res = (res + suffixes[suffixes.length - (len - i)]) % mod;
-            return (int) res;
-        } else {
-            if (len <= i + 1) {
-                long res = ((long) suffixes[i + 1 - len] - (long) suffixes[i + 1] * bases[len] % mod + mod) % mod;
-                return (int) res;
-            }
-            long res = ((long) suffixes[0] - (long) suffixes[i + 1] * bases[i + 1] % mod + mod) % mod;
-            res = (res * bases[suffixes.length - 1 - i] + suffixe_revs[i + 1]) % mod;
-            return (int) res;
-        }
+    public int dfsLinks(TreeNode node, int prv, List<List<Integer>> links, List<Integer> values) {
+        int currentIdx = links.size();
+        List<Integer> arr = new ArrayList<>();
+        links.add(arr);
+        values.add(node.val);
+        if (prv != -1)
+            arr.add(prv);
+        if (node.left != null)
+            arr.add(dfsLinks(node.left, currentIdx, links, values));
+        if (node.right != null)
+            arr.add(dfsLinks(node.right, currentIdx, links, values));
+        return currentIdx;
     }
 
-    public boolean compare(char[] chrs, int type1, int i, int type2, int j) {
-        int l = 1;
-        int r = chrs.length + 1;
-        while (l < r) {
-            int mid = (l + r) >>> 1;
-            int h1 = getH(type1, i, mid);
-            int h2 = getH(type2, j, mid);
-            if (h1 != h2) {
-                r = mid;
-            } else {
-                l = mid + 1;
+    public int dfs(int[][] links, int[] values, boolean[] seens, int i, int prv) {
+        int val = values[i];
+        if (seens[val + 1000])
+            return -100000000;
+        seens[val + 1000] = true;
+        int res = 0;
+        for (int next : links[i]) {
+            if (next != prv) {
+                res = Math.max(res, dfs(links, values, seens, next, i));
             }
         }
-        if (l == chrs.length + 1)
-            return false;
-        return getChr(chrs, type2, j, l) < getChr(chrs, type1, i, l);
-    }
-
-    public char getChr(char[] chrs, int type, int i, int len) {
-        if (type == 0) {
-            if (len <= i)
-                return chrs[len - 1];
-            return chrs[chrs.length - (len - i)];
-        } else {
-            if (len <= i + 1)
-                return chrs[i + 1 - len];
-            return chrs[len - 1];
-        }
+        seens[val + 1000] = false;
+        return Math.max(res, 0) + val;
     }
 }
