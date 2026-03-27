1f8f623a8573b2e6d539af01dc5b941b00253200
watercyb <watercyb@hotmail.com>
2026-03-27 10:39:55 -0700

    Add solution: 2946. Matrix Similarity After Cyclic Shifts

---
+++ 2946. Matrix Similarity After Cyclic Shifts/2946. Matrix Similarity After Cyclic Shifts.java
@@ -0,0 +1,19 @@
+/*
+ * Problem: 2946. Matrix Similarity After Cyclic Shifts
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/matrix-similarity-after-cyclic-shifts/?envType=daily-question&envId=2026-03-27
+ * Language: java
+ * Date: 2026-03-27
+ */
+
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] != mat[i][(j + k) % mat[0].length])
+                    return false;
+            }
+        }
+        return true;
+    }
+}
