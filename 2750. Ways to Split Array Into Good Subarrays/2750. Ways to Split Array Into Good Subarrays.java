d5733946959ceadec0c6ff93e6b0d6a7d2c0c0ee
watercyb <watercyb@hotmail.com>
2026-03-27 10:34:08 -0700

    Add solution: 2750. Ways to Split Array Into Good Subarrays

---
+++ 2750. Ways to Split Array Into Good Subarrays/2750. Ways to Split Array Into Good Subarrays.java
@@ -0,0 +1,29 @@
+/*
+ * Problem: 2750. Ways to Split Array Into Good Subarrays
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/ways-to-split-array-into-good-subarrays/?envType=problem-list-v2&envId=math
+ * Language: java
+ * Date: 2026-03-27
+ */
+
+class Solution {
+    public int numberOfGoodSubarraySplits(int[] nums) {
+        int mod = 1_000_000_007;
+        int idx = 0;
+        while (idx < nums.length && nums[idx] == 0) {
+            idx++;
+        }
+        if (idx == nums.length)
+            return 0;
+        long res = 1;
+        int prv = idx++;
+        while (idx < nums.length) {
+            if (nums[idx] == 1) {
+                res = res * (idx - prv) % mod;
+                prv = idx;
+            }
+            idx++;
+        }
+        return (int) res;
+    }
+}
