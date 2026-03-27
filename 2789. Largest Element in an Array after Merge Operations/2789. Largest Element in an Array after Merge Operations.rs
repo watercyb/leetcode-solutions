1f1c74c098c18e2250d5020f1471c5de42f36e1c
watercyb <watercyb@hotmail.com>
2026-03-27 10:38:11 -0700

    Add solution: 2789. Largest Element in an Array after Merge Operations

---
+++ 2789. Largest Element in an Array after Merge Operations/2789. Largest Element in an Array after Merge Operations.rs
@@ -0,0 +1,25 @@
+/*
+ * Problem: 2789. Largest Element in an Array after Merge Operations
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/largest-element-in-an-array-after-merge-operations/?envType=problem-list-v2&envId=greedy
+ * Language: rust
+ * Date: 2026-03-27
+ */
+
+impl Solution {
+    pub fn max_array_value(nums: Vec<i32>) -> i64 {
+        let mut res: i64=0;
+        let mut sum=nums[nums.len()-1] as i64;
+        for i in (0..nums.len()-1).rev() {
+            let num=nums[i] as i64;
+            if num<=sum {
+                sum+=num;
+            } else {
+                res=res.max(sum);
+                sum=num;
+            }
+        }
+        res=res.max(sum);
+        return res;
+    }
+}
