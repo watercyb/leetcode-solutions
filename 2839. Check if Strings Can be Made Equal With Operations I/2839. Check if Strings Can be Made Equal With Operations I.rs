d8ee1d84178bc10ca821107caa10464c011273e8
watercyb <watercyb@hotmail.com>
2026-03-28 18:44:46 -0700

    Add solution: 2839. Check if Strings Can be Made Equal With Operations I

---
+++ 2839. Check if Strings Can be Made Equal With Operations I/2839. Check if Strings Can be Made Equal With Operations I.rs
@@ -0,0 +1,27 @@
+/*
+ * Problem: 2839. Check if Strings Can be Made Equal With Operations I
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/?envType=daily-question&envId=2026-03-29
+ * Language: rust
+ * Date: 2026-03-29
+ */
+
+impl Solution {
+    pub fn can_be_equal(s1: String, s2: String) -> bool {
+        let mut counts=[[0; 26]; 2];
+        let mut idx=0;
+        for chr in s1.chars() {
+            counts[idx][(chr as u8 - b'a') as usize]+=1;
+            idx=1-idx;
+        }
+        idx=0;
+        for chr in s2.chars() {
+            counts[idx][(chr as u8 - b'a') as usize]-=1;
+            if (counts[idx][(chr as u8 - b'a') as usize]<0) {
+                return false;
+            }
+            idx=1-idx;
+        }
+        return true;
+    }
+}
