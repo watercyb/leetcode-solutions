191616e96702382106d1db8296fe7606d4814c1a
watercyb <watercyb@hotmail.com>
2026-03-28 18:44:43 -0700

    Update solution: Unknown Problem

--- Unknown Problem/Unknown Problem.java
+++ Unknown Problem/Unknown Problem.java
@@ -1,23 +1,49 @@
 /*
  * Problem: Unknown Problem
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/minimum-levels-to-gain-more-points/?envType=problem-list-v2&envId=prefix-sum
+ * Link: https://leetcode.com/problems/design-phone-directory/?envType=weekly-question&envId=2026-03-29
  * Language: java
- * Date: 2026-03-26
+ * Date: 2026-03-29
  */
 
-class Solution {
-    public int minimumLevels(int[] possible) {
-        int total = 0;
-        for (int num : possible) {
-            total += num * 2 - 1;
-        }
-        int sum = 0;
-        for (int i = 0; i < possible.length - 1; i++) {
-            sum += possible[i] * 2 - 1;
-            if (2 * sum - total > 0)
-                return i + 1;
+class PhoneDirectory {
+    boolean[] used;
+    int max = 0;
+    int[] arr = new int[20000];
+    int l = 0;
+    int r = 0;
+
+    public PhoneDirectory(int maxNumbers) {
+        used = new boolean[maxNumbers];
+    }
+
+    public int get() {
+        if (l < r) {
+            used[arr[l]] = true;
+            return arr[l++];
+        } else if (max < used.length) {
+            used[max] = true;
+            return max++;
         }
         return -1;
     }
+
+    public boolean check(int number) {
+        return !used[number];
+    }
+
+    public void release(int number) {
+        if (!used[number])
+            return;
+        used[number] = false;
+        arr[r++] = number;
+    }
 }
+
+/**
+ * Your PhoneDirectory object will be instantiated and called as such:
+ * PhoneDirectory obj = new PhoneDirectory(maxNumbers);
+ * int param_1 = obj.get();
+ * boolean param_2 = obj.check(number);
+ * obj.release(number);
+ */
