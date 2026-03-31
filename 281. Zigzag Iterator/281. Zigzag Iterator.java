3c4c247e9bda06607854d0f7b1f51fbf7bcfc2ac
watercyb <watercyb@hotmail.com>
2026-03-30 19:40:21 -0700

    Add solution: 281. Zigzag Iterator

---
+++ 281. Zigzag Iterator/281. Zigzag Iterator.java
@@ -0,0 +1,43 @@
+/*
+ * Problem: 281. Zigzag Iterator
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/zigzag-iterator/
+ * Language: java
+ * Date: 2026-03-31
+ */
+
+public class ZigzagIterator {
+    List<Integer>[] v = new ArrayList[2];
+    int[] idx = new int[2];
+    int turn = 0;
+    int count;
+
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+        v[0] = v1;
+        v[1] = v2;
+        count = v1.size() + v2.size();
+        if (count == 0)
+            return;
+        if (idx[0] == v[0].size())
+            turn = 1;
+    }
+
+    public int next() {
+        if (count-- <= 0)
+            return -1;
+        int res = v[turn].get(idx[turn]++);
+        if (idx[1 - turn] < v[1 - turn].size())
+            turn = 1 - turn;
+        return res;
+    }
+
+    public boolean hasNext() {
+        return count > 0;
+    }
+}
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i = new ZigzagIterator(v1, v2);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
