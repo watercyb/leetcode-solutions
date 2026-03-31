bfc4bdd7ee93e658d45a07a0cd69f6c01b7a9da0
watercyb <watercyb@hotmail.com>
2026-03-30 19:42:15 -0700

    Add solution: 292. Nim Game

---
+++ 292. Nim Game/292. Nim Game.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 292. Nim Game
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/nim-game/
+ * Language: java
+ * Date: 2026-03-31
+ */
+
+class Solution {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
