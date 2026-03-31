e6f688265dd7210e7580a39f709030833d6f9825
watercyb <watercyb@hotmail.com>
2026-03-31 10:59:51 -0700

    Add solution: 3308. Find Top Performing Driver

---
+++ 3308. Find Top Performing Driver/3308. Find Top Performing Driver.py
@@ -0,0 +1,18 @@
+#
+# Problem: 3308. Find Top Performing Driver
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/find-top-performing-driver/?envType=problem-list-v2&envId=ne414q67
+# Language: python3
+# Date: 2026-03-31
+
+
+import pandas as pd
+
+def get_top_performing_drivers(drivers: pd.DataFrame, vehicles: pd.DataFrame, trips: pd.DataFrame) -> pd.DataFrame:
+    vehicles=pd.merge(vehicles, trips, how='inner', on='vehicle_id')
+    vehicles=vehicles.groupby(['driver_id', 'fuel_type']).agg({'distance': 'sum', 'rating': 'mean'}).reset_index()
+    drivers=pd.merge(drivers, vehicles, how='inner', on='driver_id')
+    drivers['rating']=drivers['rating'].round(2)
+    drivers=drivers[['fuel_type', 'driver_id', 'rating', 'distance']].sort_values(by=['rating', 'distance', 'fuel_type'], ascending=[False, False, True])
+    drivers=drivers.groupby('fuel_type').first(1).reset_index()
+    return drivers
