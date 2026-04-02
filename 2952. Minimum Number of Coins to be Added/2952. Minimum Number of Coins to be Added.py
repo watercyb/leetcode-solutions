9929aad882275284e5e203f09e6e4e53b6458bc1
watercyb <watercyb@hotmail.com>
2026-04-02 12:27:48 -0700

    Add solution: 2952. Minimum Number of Coins to be Added

---
+++ 2952. Minimum Number of Coins to be Added/2952. Minimum Number of Coins to be Added.py
@@ -0,0 +1,25 @@
+#
+# Problem: 2952. Minimum Number of Coins to be Added
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/minimum-number-of-coins-to-be-added/?envType=problem-list-v2&envId=sorting
+# Language: python3
+# Date: 2026-04-02
+
+
+class Solution:
+    def minimumAddedCoins(self, coins: List[int], target: int) -> int:
+        coins.sort()
+        lim = 0
+        res = 0
+        for coin in coins:
+            while lim < coin - 1:
+                lim += lim + 1
+                res += 1
+            if lim >= target:
+                break
+            lim += coin
+        while lim < target:
+            lim += lim + 1
+            res += 1
+        return res
+
