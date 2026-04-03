e3937e0b22059a790eacf8a314f4ae7ae5d912fb
watercyb <watercyb@hotmail.com>
2026-04-02 23:57:47 -0700

    Add solution: 412. Fizz Buzz

---
+++ 412. Fizz Buzz/412. Fizz Buzz.java
@@ -0,0 +1,25 @@
+/*
+ * Problem: 412. Fizz Buzz
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/fizz-buzz/
+ * Language: java
+ * Date: 2026-04-03
+ */
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                res.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                res.add("Fizz");
+            } else if (i % 5 == 0) {
+                res.add("Buzz");
+            } else {
+                res.add(String.valueOf(i));
+            }
+        }
+        return res;
+    }
+}
