baaced3df0b27d86bd5734cbf85563c5ff031a02
watercyb <watercyb@hotmail.com>
2026-04-02 21:36:51 -0700

    Update solution: 349. Intersection of Two Arrays

--- 349. Intersection of Two Arrays/349. Intersection of Two Arrays.java
+++ 349. Intersection of Two Arrays/349. Intersection of Two Arrays.java
@@ -8,6 +8,23 @@
 
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        
+        int[] seen = new int[1001];
+        for (int num : nums1) {
+            seen[num] = 1;
+        }
+        int count = 0;
+        for (int num : nums2) {
+            if (seen[num] == 1) {
+                seen[num] = 2;
+                count++;
+            }
+        }
+        int[] res = new int[count];
+        int idx = 0;
+        for (int i = 0; i < 1001; i++) {
+            if (seen[i] == 2)
+                res[idx++] = i;
+        }
+        return res;
     }
 }
