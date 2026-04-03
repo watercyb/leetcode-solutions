baae93aaddc7433864954a1bccde8a0f7ba13b97
watercyb <watercyb@hotmail.com>
2026-04-02 21:23:18 -0700

    Add solution: 338. Counting Bits

---
+++ 338. Counting Bits/338. Counting Bits.java
@@ -0,0 +1,17 @@
+/*
+ * Problem: 338. Counting Bits
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/counting-bits/
+ * Language: java
+ * Date: 2026-04-03
+ */
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        return res;
+    }
+}
