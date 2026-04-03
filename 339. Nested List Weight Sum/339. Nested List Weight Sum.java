3043d90978fba72f4b0e1abcbd55db9545507163
watercyb <watercyb@hotmail.com>
2026-04-02 21:23:23 -0700

    Add solution: 339. Nested List Weight Sum

---
+++ 339. Nested List Weight Sum/339. Nested List Weight Sum.java
@@ -0,0 +1,57 @@
+/*
+ * Problem: 339. Nested List Weight Sum
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/nested-list-weight-sum/
+ * Language: java
+ * Date: 2026-04-03
+ */
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ * // Constructor initializes an empty nested list.
+ * public NestedInteger();
+ *
+ * // Constructor initializes a single integer.
+ * public NestedInteger(int value);
+ *
+ * // @return true if this NestedInteger holds a single integer, rather than a
+ * nested list.
+ * public boolean isInteger();
+ *
+ * // @return the single integer that this NestedInteger holds, if it holds a
+ * single integer
+ * // Return null if this NestedInteger holds a nested list
+ * public Integer getInteger();
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * public void setInteger(int value);
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to
+ * it.
+ * public void add(NestedInteger ni);
+ *
+ * // @return the nested list that this NestedInteger holds, if it holds a
+ * nested list
+ * // Return empty list if this NestedInteger holds a single integer
+ * public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        return dfs(nestedList, 1);
+    }
+
+    public int dfs(List<NestedInteger> nestedList, int depth) {
+        int res = 0;
+        for (NestedInteger nestedInteger : nestedList) {
+            if (nestedInteger.isInteger()) {
+                res += nestedInteger.getInteger() * depth;
+            } else {
+                res += dfs(nestedInteger.getList(), depth + 1);
+            }
+        }
+        return res;
+    }
+}
