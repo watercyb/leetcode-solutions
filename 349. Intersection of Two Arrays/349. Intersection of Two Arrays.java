a862b584da8cfea2747bf2ed8c2784eb278bd9d1
watercyb <watercyb@hotmail.com>
2026-04-02 21:36:37 -0700

    Add solution: 349. Intersection of Two Arrays

---
+++ 349. Intersection of Two Arrays/349. Intersection of Two Arrays.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 349. Intersection of Two Arrays
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/intersection-of-two-arrays/
+ * Language: java
+ * Date: 2026-04-03
+ */
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        
+    }
+}
