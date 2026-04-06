c67e3f7a672b53360243c295c4561bc4009868ff
watercyb <watercyb@hotmail.com>
2026-04-05 21:50:12 -0700

    Add solution: 669. Trim a Binary Search Tree

---
+++ 669. Trim a Binary Search Tree/669. Trim a Binary Search Tree.java
@@ -0,0 +1,38 @@
+/*
+ * Problem: 669. Trim a Binary Search Tree
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/trim-a-binary-search-tree/
+ * Language: java
+ * Date: 2026-04-06
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null)
+            return root;
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        } else if (root.val > high) {
+            return trimBST(root.left, low, high);
+        } else {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+            return root;
+        }
+    }
+}
