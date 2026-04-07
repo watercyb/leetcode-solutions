ce122b9bd0379c15c3a6b75c537d746ee68cc3ff
watercyb <watercyb@hotmail.com>
2026-04-07 09:39:40 -0700

    Add solution: 3384. Team Dominance by Pass Success

---
+++ 3384. Team Dominance by Pass Success/3384. Team Dominance by Pass Success.sql
@@ -0,0 +1,16 @@
+/*
+ * Problem: 3384. Team Dominance by Pass Success
+ * Difficulty: Hard
+ * Link: https://leetcode.com/problems/team-dominance-by-pass-success/?envType=problem-list-v2&envId=ne414q67
+ * Language: mysql
+ * Date: 2026-04-07
+ */
+
+# Write your MySQL query statement below
+with total as (select b.team_name, case when a.time_stamp<='45:00' then 1 else 2 end as half_number, case when b.team_name=c.team_name then 1 else -1 end as dominance
+from Passes as a, Teams as b, Teams as c
+where a.pass_from=b.player_id and a.pass_to=c.player_id)
+select team_name, half_number, SUM(dominance) as dominance
+from total
+group by team_name, half_number
+order by team_name, half_number
