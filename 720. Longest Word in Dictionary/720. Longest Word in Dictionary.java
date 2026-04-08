a244ce1c375d30900326fc74fb2efa349d7c78d2
watercyb <watercyb@hotmail.com>
2026-04-07 21:17:25 -0700

    Add solution: 720. Longest Word in Dictionary

---
+++ 720. Longest Word in Dictionary/720. Longest Word in Dictionary.java
@@ -0,0 +1,45 @@
+/*
+ * Problem: 720. Longest Word in Dictionary
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/longest-word-in-dictionary/
+ * Language: java
+ * Date: 2026-04-08
+ */
+
+class Solution {
+    public String longestWord(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String word : words) {
+            insert(root, word);
+        }
+        dfs(root);
+        return res;
+    }
+
+    String res = "";
+
+    public void insert(TrieNode node, String word) {
+        char[] chrs = word.toCharArray();
+        for (int i = 0; i < chrs.length; i++) {
+            int idx = chrs[i] - 'a';
+            if (node.next[idx] == null)
+                node.next[idx] = new TrieNode();
+            node = node.next[idx];
+        }
+        node.word = word;
+    }
+
+    public void dfs(TrieNode node) {
+        if (node.word.length() > res.length())
+            res = node.word;
+        for (int i = 0; i < 26; i++) {
+            if (node.next[i] != null && !node.next[i].word.isEmpty())
+                dfs(node.next[i]);
+        }
+    }
+}
+
+class TrieNode {
+    TrieNode[] next = new TrieNode[26];
+    String word = "";
+}
