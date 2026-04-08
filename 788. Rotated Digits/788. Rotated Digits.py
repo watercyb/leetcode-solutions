c1e66a482db4f7e9e0fbcb7527cdb4d60e5f1fdc
watercyb <watercyb@hotmail.com>
2026-04-07 21:37:02 -0700

    Add solution: 788. Rotated Digits

---
+++ 788. Rotated Digits/788. Rotated Digits.py
@@ -0,0 +1,21 @@
+#
+# Problem: 788. Rotated Digits
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/rotated-digits/
+# Language: python3
+# Date: 2026-04-08
+
+
+class Solution:
+    def rotatedDigits(self, n: int) -> int:
+        arr = [1, 1, 2, 4, 4, 2, 2, 4, 1, 2]
+        res = 0
+        for i in range(1, n + 1):
+            h = 0
+            while i > 0:
+                h |= arr[i % 10]
+                i //= 10
+            if h == 2 or h == 3:
+                res += 1
+        return res
+
