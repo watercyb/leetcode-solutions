7bc114bde7adb73da4dfeacf16fd2167a3c5f3e7
watercyb <watercyb@hotmail.com>
2026-04-08 23:01:25 -0700

    Add solution: 894. All Possible Full Binary Trees

---
+++ 894. All Possible Full Binary Trees/894. All Possible Full Binary Trees.java
@@ -0,0 +1,28 @@
+/*
+ * Problem: 894. All Possible Full Binary Trees
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/all-possible-full-binary-trees/
+ * Language: java
+ * Date: 2026-04-09
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> allPossibleFBT(int n) {
+        
+    }
+}
