10f5301a4f74dcf0c372cd8cfb4b58b6ba685f23
watercyb <watercyb@hotmail.com>
2026-04-08 22:58:10 -0700

    Add solution: 877. Stone Game

---
+++ 877. Stone Game/877. Stone Game.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 877. Stone Game
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/stone-game/
+ * Language: java
+ * Date: 2026-04-09
+ */
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
