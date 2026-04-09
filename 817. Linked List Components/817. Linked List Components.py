e134d164f26e3218112f06fc593560a87096d96f
watercyb <watercyb@hotmail.com>
2026-04-08 22:41:09 -0700

    Add solution: 817. Linked List Components

---
+++ 817. Linked List Components/817. Linked List Components.py
@@ -0,0 +1,16 @@
+#
+# Problem: 817. Linked List Components
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/linked-list-components/
+# Language: python3
+# Date: 2026-04-09
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+        
