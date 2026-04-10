5463de9ece63826bbdac2dec2c2ea82639ca7fd8
watercyb <watercyb@hotmail.com>
2026-04-09 20:34:16 -0700

    Add solution: 992. Subarrays with K Different Integers

---
+++ 992. Subarrays with K Different Integers/992. Subarrays with K Different Integers.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 992. Subarrays with K Different Integers
+ * Difficulty: Hard
+ * Link: https://leetcode.com/problems/subarrays-with-k-different-integers/
+ * Language: java
+ * Date: 2026-04-10
+ */
+
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        
+    }
+}
