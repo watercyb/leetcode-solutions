2158182aba9af3aada1aab4daa24199ffb365e18
watercyb <watercyb@hotmail.com>
2026-04-10 22:40:51 -0700

    Add solution: 1020. Number of Enclaves

---
+++ 1020. Number of Enclaves/1020. Number of Enclaves.java
@@ -0,0 +1,45 @@
+/*
+ * Problem: 1020. Number of Enclaves
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/number-of-enclaves/
+ * Language: java
+ * Date: 2026-04-11
+ */
+
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        int r = grid.length - 1;
+        int l = grid[0].length - 1;
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[i][0] == 1)
+                dfs(grid, i, 0);
+            if (grid[i][l] == 1)
+                dfs(grid, i, l);
+        }
+        for (int i = 1; i < l; i++) {
+            if (grid[0][i] == 1)
+                dfs(grid, 0, i);
+            if (grid[r][i] == 1)
+                dfs(grid, r, i);
+        }
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                res += grid[i][j];
+            }
+        }
+        return res;
+    }
+
+    public void dfs(int[][] grid, int i, int j) {
+        grid[i][j] = 0;
+        if (i > 0 && grid[i - 1][j] == 1)
+            dfs(grid, i - 1, j);
+        if (i < grid.length - 1 && grid[i + 1][j] == 1)
+            dfs(grid, i + 1, j);
+        if (j > 0 && grid[i][j - 1] == 1)
+            dfs(grid, i, j - 1);
+        if (j < grid[0].length - 1 && grid[i][j + 1] == 1)
+            dfs(grid, i, j + 1);
+    }
+}
