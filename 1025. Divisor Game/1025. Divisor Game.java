60bbe76854a66133b8cb8bff625250e1307bf9b6
watercyb <watercyb@hotmail.com>
2026-04-10 22:42:16 -0700

    Add solution: 1025. Divisor Game

---
+++ 1025. Divisor Game/1025. Divisor Game.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 1025. Divisor Game
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/divisor-game/
+ * Language: java
+ * Date: 2026-04-11
+ */
+
+class Solution {
+    public boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+}
