0f1d8fe803d210b95b58d2675c376807cbd78ea2
watercyb <watercyb@hotmail.com>
2026-04-13 22:05:43 -0700

    Add solution: 3898. Find the Degree of Each Vertex

---
+++ 3898. Find the Degree of Each Vertex/3898. Find the Degree of Each Vertex.py
@@ -0,0 +1,15 @@
+#
+# Problem: 3898. Find the Degree of Each Vertex
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/find-the-degree-of-each-vertex/
+# Language: python3
+# Date: 2026-04-14
+
+
+class Solution:
+    def findDegrees(self, matrix: list[list[int]]) -> list[int]:
+        res = []
+        for row in matrix:
+            res.append(sum(row))
+        return res
+
