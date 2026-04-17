eaae89a1ddbedef659ee9c90ae47181b9b29722d
watercyb <watercyb@hotmail.com>
2026-04-16 22:56:33 -0700

    Add solution: 1476. Subrectangle Queries

---
+++ 1476. Subrectangle Queries/1476. Subrectangle Queries.java
@@ -0,0 +1,34 @@
+/*
+ * Problem: 1476. Subrectangle Queries
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/subrectangle-queries/
+ * Language: java
+ * Date: 2026-04-17
+ */
+
+class SubrectangleQueries {
+    int[][] rectangle;
+
+    public SubrectangleQueries(int[][] rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (int i = row1; i <= row2; i++) {
+            for (int j = col1; j <= col2; j++) {
+                rectangle[i][j] = newValue;
+            }
+        }
+    }
+
+    public int getValue(int row, int col) {
+        return rectangle[row][col];
+    }
+}
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * SubrectangleQueries obj = new SubrectangleQueries(rectangle);
+ * obj.updateSubrectangle(row1,col1,row2,col2,newValue);
+ * int param_2 = obj.getValue(row,col);
+ */
