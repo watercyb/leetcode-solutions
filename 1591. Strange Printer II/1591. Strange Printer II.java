d1fc308c6c35964fbd9f125f6301e31737095fe0
watercyb <watercyb@hotmail.com>
2026-04-17 22:55:27 -0700

    Add solution: 1591. Strange Printer II

---
+++ 1591. Strange Printer II/1591. Strange Printer II.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 1591. Strange Printer II
+ * Difficulty: Hard
+ * Link: https://leetcode.com/problems/strange-printer-ii/
+ * Language: java
+ * Date: 2026-04-18
+ */
+
+class Solution {
+    public boolean isPrintable(int[][] targetGrid) {
+        
+    }
+}
