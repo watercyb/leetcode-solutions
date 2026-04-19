f991d3aaae8d05494153b754f1c508aba7842978
watercyb <watercyb@hotmail.com>
2026-04-18 20:35:03 -0700

    Add solution: 1609. Even Odd Tree

---
+++ 1609. Even Odd Tree/1609. Even Odd Tree.java
@@ -0,0 +1,28 @@
+/*
+ * Problem: 1609. Even Odd Tree
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/even-odd-tree/
+ * Language: java
+ * Date: 2026-04-19
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        
+    }
+}
