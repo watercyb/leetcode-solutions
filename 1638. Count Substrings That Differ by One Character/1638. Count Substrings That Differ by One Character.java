9dafbde396c5776a7f9144a8b32c6f4d6cbec2f4
watercyb <watercyb@hotmail.com>
2026-04-18 20:49:09 -0700

    Add solution: 1638. Count Substrings That Differ by One Character

---
+++ 1638. Count Substrings That Differ by One Character/1638. Count Substrings That Differ by One Character.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 1638. Count Substrings That Differ by One Character
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/count-substrings-that-differ-by-one-character/
+ * Language: java
+ * Date: 2026-04-19
+ */
+
+class Solution {
+    public int countSubstrings(String s, String t) {
+        
+    }
+}
