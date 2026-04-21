f232802206851af1e550938b23eabd9b9b08a8e5
watercyb <watercyb@hotmail.com>
2026-04-20 22:11:24 -0700

    Update solution: 1848. Minimum Distance to the Target Element

--- 1848. Minimum Distance to the Target Element/1848. Minimum Distance to the Target Element.rs
+++ 1848. Minimum Distance to the Target Element/1848. Minimum Distance to the Target Element.rs
@@ -1,9 +1,9 @@
 /*
  * Problem: 1848. Minimum Distance to the Target Element
  * Difficulty: Easy
- * Link: https://leetcode.com/problems/minimum-distance-to-the-target-element/?envType=daily-question&envId=2026-04-13
+ * Link: https://leetcode.com/problems/minimum-distance-to-the-target-element/
  * Language: rust
- * Date: 2026-04-13
+ * Date: 2026-04-21
  */
 
 impl Solution {
