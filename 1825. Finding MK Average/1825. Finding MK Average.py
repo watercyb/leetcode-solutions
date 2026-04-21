2c0847872cc7395ec0faae1a0701a60ebb0699b0
watercyb <watercyb@hotmail.com>
2026-04-20 22:07:14 -0700

    Add solution: 1825. Finding MK Average

---
+++ 1825. Finding MK Average/1825. Finding MK Average.py
@@ -0,0 +1,24 @@
+#
+# Problem: 1825. Finding MK Average
+# Difficulty: Hard
+# Link: https://leetcode.com/problems/finding-mk-average/
+# Language: python3
+# Date: 2026-04-21
+
+
+class MKAverage:
+
+    def __init__(self, m: int, k: int):
+        
+
+    def addElement(self, num: int) -> None:
+        
+
+    def calculateMKAverage(self) -> int:
+        
+
+
+# Your MKAverage object will be instantiated and called as such:
+# obj = MKAverage(m, k)
+# obj.addElement(num)
+# param_2 = obj.calculateMKAverage()
