f2690004d0404bae4781e30ad15aa4fd40905088
watercyb <watercyb@hotmail.com>
2026-04-24 22:00:15 -0700

    Add solution: 2235. Add Two Integers

---
+++ 2235. Add Two Integers/2235. Add Two Integers.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 2235. Add Two Integers
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/add-two-integers/
+ * Language: java
+ * Date: 2026-04-25
+ */
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
