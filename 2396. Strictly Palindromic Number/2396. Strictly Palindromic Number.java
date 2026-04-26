548efbe8699e90e7019f4a49d66457d3f1dff5aa
watercyb <watercyb@hotmail.com>
2026-04-25 23:16:15 -0700

    Add solution: 2396. Strictly Palindromic Number

---
+++ 2396. Strictly Palindromic Number/2396. Strictly Palindromic Number.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 2396. Strictly Palindromic Number
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/strictly-palindromic-number/
+ * Language: java
+ * Date: 2026-04-26
+ */
+
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+}
