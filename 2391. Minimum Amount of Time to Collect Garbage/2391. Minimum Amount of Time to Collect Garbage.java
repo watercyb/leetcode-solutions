ee2433734f7c5e7f2c25798e26b04f842b10c3fb
watercyb <watercyb@hotmail.com>
2026-04-25 23:15:09 -0700

    Add solution: 2391. Minimum Amount of Time to Collect Garbage

---
+++ 2391. Minimum Amount of Time to Collect Garbage/2391. Minimum Amount of Time to Collect Garbage.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 2391. Minimum Amount of Time to Collect Garbage
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/
+ * Language: java
+ * Date: 2026-04-26
+ */
+
+class Solution {
+    public int garbageCollection(String[] garbage, int[] travel) {
+        
+    }
+}
