aa562fb7c8cd4a22cda07867a48a48c28676f9a1
watercyb <watercyb@hotmail.com>
2026-04-28 12:49:23 -0700

    Add solution: 3912. Valid Elements in an Array

---
+++ 3912. Valid Elements in an Array/3912. Valid Elements in an Array.rs
@@ -0,0 +1,36 @@
+/*
+ * Problem: 3912. Valid Elements in an Array
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/valid-elements-in-an-array/
+ * Language: rust
+ * Date: 2026-04-28
+ */
+
+impl Solution {
+    pub fn find_valid_elements(nums: Vec<i32>) -> Vec<i32> {
+        let mut arr: Vec<bool>=Vec::new();
+        let mut max=0;
+        for i in 0..nums.len() {
+            if nums[i]>max {
+                arr.push(true);
+                max=nums[i];
+            } else {
+                arr.push(false);
+            }
+        }
+        max=0;
+        for i in (0..nums.len()).rev() {
+            if nums[i]>max {
+                arr[i]=true;
+                max=nums[i];
+            }
+        }
+        let mut res: Vec<i32>=Vec::new();
+        for i in 0..nums.len() {
+            if arr[i] {
+                res.push(nums[i]);
+            }
+        }
+        return res;
+    }
+}
