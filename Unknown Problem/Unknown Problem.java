9b9fec1bd3230b5a0dbbe243bd94a859bdd96c71
watercyb <watercyb@hotmail.com>
2026-04-28 21:31:31 -0700

    Update solution: Unknown Problem

--- Unknown Problem/Unknown Problem.java
+++ Unknown Problem/Unknown Problem.java
@@ -1,49 +1,48 @@
 /*
  * Problem: Unknown Problem
- * Difficulty: Medium
- * Link: https://leetcode.com/problems/design-phone-directory/?envType=weekly-question&envId=2026-03-29
+ * Difficulty: Hard
+ * Link: https://leetcode.com/problems/maximum-score-from-grid-operations/?envType=daily-question&envId=2026-04-29
  * Language: java
- * Date: 2026-03-29
+ * Date: 2026-04-29
  */
 
-class PhoneDirectory {
-    boolean[] used;
-    int max = 0;
-    int[] arr = new int[20000];
-    int l = 0;
-    int r = 0;
-
-    public PhoneDirectory(int maxNumbers) {
-        used = new boolean[maxNumbers];
-    }
-
-    public int get() {
-        if (l < r) {
-            used[arr[l]] = true;
-            return arr[l++];
-        } else if (max < used.length) {
-            used[max] = true;
-            return max++;
+class Solution {
+    public long maximumScore(int[][] grid) {
+        int n = grid.length;
+        long[][] sums = new long[grid.length][grid.length + 1];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                sums[i][j + 1] = sums[i][j] + grid[j][i];
+            }
         }
-        return -1;
-    }
-
-    public boolean check(int number) {
-        return !used[number];
-    }
-
-    public void release(int number) {
-        if (!used[number])
-            return;
-        used[number] = false;
-        arr[r++] = number;
+        long[] DP1 = new long[n + 1];
+        long[] DP2 = new long[n + 1];
+        for (int i = 1; i < grid.length; i++) {
+            long[] DP1Next = new long[n + 1];
+            long[] DP2Next = new long[n + 1];
+            long max = 0;
+            for (int j = 0; j <= n; j++) {
+                max = Math.max(DP1[j], max);
+                DP1Next[j] = max;
+                DP2Next[j] = max;
+            }
+            for (int j = 0; j <= n; j++) {
+                for (int k = 0; k < j; k++) {
+                    DP1Next[k] = Math.max(DP1[j] + sums[i][j] - sums[i][k], DP1Next[k]);
+                }
+                for (int k = j + 1; k <= n; k++) {
+                    long sum = DP2[j] + sums[i - 1][k] - sums[i - 1][j];
+                    DP1Next[k] = Math.max(sum, DP1Next[k]);
+                    DP2Next[k] = Math.max(sum, DP2Next[k]);
+                }
+            }
+            DP1 = DP1Next;
+            DP2 = DP2Next;
+        }
+        long res = 0;
+        for (int i = 0; i <= n; i++) {
+            res = Math.max(DP1[i], res);
+        }
+        return res;
     }
 }
-
-/**
- * Your PhoneDirectory object will be instantiated and called as such:
- * PhoneDirectory obj = new PhoneDirectory(maxNumbers);
- * int param_1 = obj.get();
- * boolean param_2 = obj.check(number);
- * obj.release(number);
- */
