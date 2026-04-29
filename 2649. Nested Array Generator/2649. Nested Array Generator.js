98c2005f5ac018c6e9e04032d7202649f09edaa4
watercyb <watercyb@hotmail.com>
2026-04-28 23:18:45 -0700

    Add solution: 2649. Nested Array Generator

---
+++ 2649. Nested Array Generator/2649. Nested Array Generator.js
@@ -0,0 +1,28 @@
+/*
+ * Problem: 2649. Nested Array Generator
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/nested-array-generator/
+ * Language: javascript
+ * Date: 2026-04-29
+ */
+
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function* (arr) {
+    if (!Array.isArray(arr)) {
+        yield arr;
+    } else {
+        for (let i = 0; i < arr.length; i++) {
+            yield* inorderTraversal(arr[i]);
+        }
+    }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
