e8d59bb19817282cf2e4b9a70153956a1c862afd
watercyb <watercyb@hotmail.com>
2026-04-29 22:05:04 -0700

    Update solution: Unknown Problem

--- Unknown Problem/Unknown Problem.py
+++ Unknown Problem/Unknown Problem.py
@@ -1,20 +1,16 @@
 #
 # Problem: Unknown Problem
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/find-overlapping-shifts-ii/?envType=problem-list-v2&envId=ne414q67
+# Link: https://leetcode.com/problems/find-the-value-of-the-partition/
 # Language: python3
-# Date: 2026-03-26
+# Date: 2026-04-30
 
 
-import pandas as pd
+class Solution:
+    def findValueOfPartition(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 1000000000
+        for i in range(1, len(nums)):
+            res = min(res, nums[i] - nums[i - 1])
+        return res
 
-def calculate_shift_overlaps(employee_shifts: pd.DataFrame) -> pd.DataFrame:
-    employee_shifts=employee_shifts.sort_values(by='start_time')
-    employee_shifts['r'] = employee_shifts.groupby('employee_id').cumcount() + 1
-    employee_shifts=pd.merge(employee_shifts, employee_shifts, how='inner', on='employee_id')
-    employee_shifts=employee_shifts[(employee_shifts['r_x']<=employee_shifts['r_y']) & (employee_shifts['end_time_x']>employee_shifts['start_time_y'])]
-    employee_shifts['total_overlap_duration']=employee_shifts.apply(lambda row: (row['end_time_x']-row['start_time_y']).total_seconds()//60 if row['r_x']!=row['r_y'] else 0, axis=1)
-    employee_shifts['max_overlapping_shifts']=employee_shifts['r_y']-employee_shifts['r_x']
-    employee_shifts=employee_shifts.groupby('employee_id').agg({'max_overlapping_shifts': 'max', 'total_overlap_duration': 'sum'}).reset_index()
-    employee_shifts['max_overlapping_shifts']=employee_shifts['max_overlapping_shifts']+1
-    return employee_shifts
