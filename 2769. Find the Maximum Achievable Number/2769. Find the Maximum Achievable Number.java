84dd34765b65db0e67a7aed8e352a1873c9f4d33
watercyb <watercyb@hotmail.com>
2026-04-29 22:17:03 -0700

    Add solution: 2769. Find the Maximum Achievable Number

---
+++ 2769. Find the Maximum Achievable Number/2769. Find the Maximum Achievable Number.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 2769. Find the Maximum Achievable Number
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/find-the-maximum-achievable-number/
+ * Language: java
+ * Date: 2026-04-30
+ */
+
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+}
