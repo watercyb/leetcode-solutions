735e973c50c2c984f4245530c41f5decc3afdacc
watercyb <watercyb@hotmail.com>
2026-04-29 21:57:35 -0700

    Add solution: 2726. Calculator with Method Chaining

---
+++ 2726. Calculator with Method Chaining/2726. Calculator with Method Chaining.js
@@ -0,0 +1,70 @@
+/*
+ * Problem: 2726. Calculator with Method Chaining
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/calculator-with-method-chaining/
+ * Language: javascript
+ * Date: 2026-04-30
+ */
+
+class Calculator {
+
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+        this.val = value;
+    }
+
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value) {
+        this.val += value;
+        return this;
+    }
+
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value) {
+        this.val -= value;
+        return this;
+    }
+
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    multiply(value) {
+        this.val *= value;
+        return this;
+    }
+
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        if (value === 0) throw 'Division by zero is not allowed';
+        this.val /= value;
+        return this;
+    }
+
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        this.val = Math.pow(this.val, value);
+        return this;
+    }
+
+    /** 
+     * @return {number}
+     */
+    getResult() {
+        return this.val;
+    }
+}
