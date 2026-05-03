562d6f8100455ae1ae28c519e3d24946c258e9ed
watercyb <watercyb@hotmail.com>
2026-05-02 17:33:43 -0700

    Update solution: 796. Rotate String

--- 796. Rotate String/796. Rotate String.java
+++ 796. Rotate String/796. Rotate String.java
@@ -1,9 +1,9 @@
 /*
  * Problem: 796. Rotate String
  * Difficulty: Easy
- * Link: https://leetcode.com/problems/rotate-string/
+ * Link: https://leetcode.com/problems/rotate-string/?envType=daily-question&envId=2026-05-03
  * Language: java
- * Date: 2026-04-08
+ * Date: 2026-05-03
  */
 
 class Solution {
