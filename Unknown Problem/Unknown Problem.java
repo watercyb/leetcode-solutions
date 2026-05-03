1c0cbb78edd9a89d3c88aab49f88472e9d61560c
watercyb <watercyb@hotmail.com>
2026-05-02 19:02:04 -0700

    Update solution: Unknown Problem

--- Unknown Problem/Unknown Problem.java
+++ Unknown Problem/Unknown Problem.java
@@ -1,48 +1,40 @@
 /*
  * Problem: Unknown Problem
- * Difficulty: Hard
- * Link: https://leetcode.com/problems/maximum-score-from-grid-operations/?envType=daily-question&envId=2026-04-29
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/most-frequent-prime/
  * Language: java
- * Date: 2026-04-29
+ * Date: 2026-05-03
  */
 
 class Solution {
-    public long maximumScore(int[][] grid) {
-        int n = grid.length;
-        long[][] sums = new long[grid.length][grid.length + 1];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
-                sums[i][j + 1] = sums[i][j] + grid[j][i];
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        TrieNode root = new TrieNode();
+        for (int num : arr1) {
+            TrieNode node = root;
+            for (char chr : String.valueOf(num).toCharArray()) {
+                chr -= '0';
+                if (node.next[chr] == null)
+                    node.next[chr] = new TrieNode();
+                node = node.next[chr];
             }
         }
-        long[] DP1 = new long[n + 1];
-        long[] DP2 = new long[n + 1];
-        for (int i = 1; i < grid.length; i++) {
-            long[] DP1Next = new long[n + 1];
-            long[] DP2Next = new long[n + 1];
-            long max = 0;
-            for (int j = 0; j <= n; j++) {
-                max = Math.max(DP1[j], max);
-                DP1Next[j] = max;
-                DP2Next[j] = max;
+        int res = 0;
+        for (int num : arr2) {
+            TrieNode node = root;
+            int count = 0;
+            for (char chr : String.valueOf(num).toCharArray()) {
+                chr -= '0';
+                if (node.next[chr] == null)
+                    break;
+                node = node.next[chr];
+                count++;
             }
-            for (int j = 0; j <= n; j++) {
-                for (int k = 0; k < j; k++) {
-                    DP1Next[k] = Math.max(DP1[j] + sums[i][j] - sums[i][k], DP1Next[k]);
-                }
-                for (int k = j + 1; k <= n; k++) {
-                    long sum = DP2[j] + sums[i - 1][k] - sums[i - 1][j];
-                    DP1Next[k] = Math.max(sum, DP1Next[k]);
-                    DP2Next[k] = Math.max(sum, DP2Next[k]);
-                }
-            }
-            DP1 = DP1Next;
-            DP2 = DP2Next;
-        }
-        long res = 0;
-        for (int i = 0; i <= n; i++) {
-            res = Math.max(DP1[i], res);
+            res = Math.max(count, res);
         }
         return res;
     }
+}
+
+class TrieNode {
+    TrieNode[] next = new TrieNode[10];
 }
