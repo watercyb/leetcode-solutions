69639dc5a81a98dbfa5a674db4c6eee4fc5e651e
watercyb <watercyb@hotmail.com>
2026-05-02 19:01:49 -0700

    Add solution: 3042. Count Prefix and Suffix Pairs I

---
+++ 3042. Count Prefix and Suffix Pairs I/3042. Count Prefix and Suffix Pairs I.java
@@ -0,0 +1,20 @@
+/*
+ * Problem: 3042. Count Prefix and Suffix Pairs I
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/
+ * Language: java
+ * Date: 2026-05-03
+ */
+
+class Solution {
+    public int countPrefixSuffixPairs(String[] words) {
+        int res = 0;
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[j].startsWith(words[i]) && words[j].endsWith(words[i]))
+                    res++;
+            }
+        }
+        return res;
+    }
+}
