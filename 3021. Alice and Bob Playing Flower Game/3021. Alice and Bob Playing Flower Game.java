4bc0d9f906c0ed314c36b6595db051015da86ab5
watercyb <watercyb@hotmail.com>
2026-05-02 18:56:56 -0700

    Add solution: 3021. Alice and Bob Playing Flower Game

---
+++ 3021. Alice and Bob Playing Flower Game/3021. Alice and Bob Playing Flower Game.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 3021. Alice and Bob Playing Flower Game
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+ * Language: java
+ * Date: 2026-05-03
+ */
+
+class Solution {
+    public long flowerGame(int n, int m) {
+        return (long) n * m / 2;
+    }
+}
