7b71c212d2b60894c360f4bb98bcec688548c59c
watercyb <watercyb@hotmail.com>
2026-05-05 23:12:18 -0700

    Add solution: 3346. Maximum Frequency of an Element After Performing Operations I

---
+++ 3346. Maximum Frequency of an Element After Performing Operations I/3346. Maximum Frequency of an Element After Performing Operations I.py
@@ -0,0 +1,11 @@
+#
+# Problem: 3346. Maximum Frequency of an Element After Performing Operations I
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/maximum-frequency-of-an-element-after-performing-operations-i/
+# Language: python3
+# Date: 2026-05-06
+
+
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+        
