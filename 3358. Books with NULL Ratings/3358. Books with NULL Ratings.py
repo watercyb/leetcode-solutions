ccdcba61e93ee72c4c2307b2fcc8ade1a95ac595
watercyb <watercyb@hotmail.com>
2026-05-05 23:14:47 -0700

    Update solution: 3358. Books with NULL Ratings

--- 3358. Books with NULL Ratings/3358. Books with NULL Ratings.py
+++ 3358. Books with NULL Ratings/3358. Books with NULL Ratings.py
@@ -1,9 +1,9 @@
 #
 # Problem: 3358. Books with NULL Ratings
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/books-with-null-ratings/?envType=problem-list-v2&envId=ne414q67
+# Link: https://leetcode.com/problems/books-with-null-ratings/
 # Language: python3
-# Date: 2026-04-03
+# Date: 2026-05-06
 
 
 import pandas as pd
