f1166f5fe18ebf1781b95a08e5193e6da10c3c77
watercyb <watercyb@hotmail.com>
2026-05-05 23:18:14 -0700

    Add solution: 3376. Minimum Time to Break Locks I

---
+++ 3376. Minimum Time to Break Locks I/3376. Minimum Time to Break Locks I.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 3376. Minimum Time to Break Locks I
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/minimum-time-to-break-locks-i/
+ * Language: java
+ * Date: 2026-05-06
+ */
+
+class Solution {
+    public int findMinimumTime(List<Integer> strength, int k) {
+        
+    }
+}
