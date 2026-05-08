66b95828b857e6cad0b4dce2524fb72d38eb2224
watercyb <watercyb@hotmail.com>
2026-05-07 22:05:29 -0700

    Update solution: 3580. Find Consistently Improving Employees

--- 3580. Find Consistently Improving Employees/3580. Find Consistently Improving Employees.sql
+++ 3580. Find Consistently Improving Employees/3580. Find Consistently Improving Employees.sql
@@ -1,9 +1,9 @@
 /*
  * Problem: 3580. Find Consistently Improving Employees
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/find-consistently-improving-employees/?envType=problem-list-v2&envId=ne414q67
+ * Link: https://leetcode.com/problems/find-consistently-improving-employees/
  * Language: mysql
- * Date: 2026-04-13
+ * Date: 2026-05-08
  */
 
 # Write your MySQL query statement below
