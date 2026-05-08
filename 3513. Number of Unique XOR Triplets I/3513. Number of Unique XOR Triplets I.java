bed6846d5e8719addbc5252faa6f0ab54325ccde
watercyb <watercyb@hotmail.com>
2026-05-07 21:55:10 -0700

    Add solution: 3513. Number of Unique XOR Triplets I

---
+++ 3513. Number of Unique XOR Triplets I/3513. Number of Unique XOR Triplets I.java
@@ -0,0 +1,18 @@
+/*
+ * Problem: 3513. Number of Unique XOR Triplets I
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/number-of-unique-xor-triplets-i/
+ * Language: java
+ * Date: 2026-05-08
+ */
+
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        if (n <= 2)
+            return n;
+        if ((n & -n) == n)
+            return 2 * n;
+        return (int) Math.pow(2, Math.ceil(Math.log(n) / Math.log(2)));
+    }
+}
