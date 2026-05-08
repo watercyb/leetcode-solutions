1933ba69af89a7e505bb3590837d808ff3bbe3ae
watercyb <watercyb@hotmail.com>
2026-05-07 21:59:28 -0700

    Add solution: 3536. Maximum Product of Two Digits

---
+++ 3536. Maximum Product of Two Digits/3536. Maximum Product of Two Digits.java
@@ -0,0 +1,30 @@
+/*
+ * Problem: 3536. Maximum Product of Two Digits
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/maximum-product-of-two-digits/
+ * Language: java
+ * Date: 2026-05-08
+ */
+
+class Solution {
+    public int maxProduct(int n) {
+        int[] counts = new int[10];
+        while (n > 0) {
+            counts[n % 10]++;
+            n /= 10;
+        }
+        int a = 0;
+        for (int i = 9; i >= 0; i--) {
+            if (counts[i] > 0) {
+                if (a == 0) {
+                    a = i;
+                    if (counts[i] > 1)
+                        return i * i;
+                } else {
+                    return a * i;
+                }
+            }
+        }
+        return 0;
+    }
+}
