d2d2afa2a08fe5cfe7ee1978db16351b9d240944
watercyb <watercyb@hotmail.com>
2026-05-08 22:29:10 -0700

    Add solution: 3612. Process String with Special Operations I

---
+++ 3612. Process String with Special Operations I/3612. Process String with Special Operations I.java
@@ -0,0 +1,26 @@
+/*
+ * Problem: 3612. Process String with Special Operations I
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/process-string-with-special-operations-i/
+ * Language: java
+ * Date: 2026-05-09
+ */
+
+class Solution {
+    public String processStr(String s) {
+        StringBuilder SB = new StringBuilder();
+        for (char chr : s.toCharArray()) {
+            if (chr == '*') {
+                if (SB.length() > 0)
+                    SB.setLength(SB.length() - 1);
+            } else if (chr == '#') {
+                SB.append(SB.toString());
+            } else if (chr == '%') {
+                SB.reverse();
+            } else {
+                SB.append(chr);
+            }
+        }
+        return SB.toString();
+    }
+}
