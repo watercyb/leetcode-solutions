757f7d3a675dc42814775619bff4a5f415e57b43
watercyb <watercyb@hotmail.com>
2026-05-08 22:41:27 -0700

    Add solution: 3680. Generate Schedule

---
+++ 3680. Generate Schedule/3680. Generate Schedule.java
@@ -0,0 +1,32 @@
+/*
+ * Problem: 3680. Generate Schedule
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/generate-schedule/
+ * Language: java
+ * Date: 2026-05-09
+ */
+
+class Solution {
+    public int minArrivalsToDiscard(int[] arrivals, int w, int m) {
+        int max = 0;
+        for (int arrival : arrivals) {
+            max = Math.max(max, arrival);
+        }
+        int[] counts = new int[max + 1];
+        int j = 0;
+        int res = 0;
+        for (int i = 0; j < arrivals.length; i++) {
+            while (j < arrivals.length && j - i < w) {
+                if (counts[arrivals[j]] == m) {
+                    res++;
+                    arrivals[j] = 0;
+                } else {
+                    counts[arrivals[j]]++;
+                }
+                j++;
+            }
+            counts[arrivals[i]]--;
+        }
+        return res;
+    }
+}
