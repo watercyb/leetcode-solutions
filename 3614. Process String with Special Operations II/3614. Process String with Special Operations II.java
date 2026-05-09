108928456bb2abb85b28d53fde4434dfa91a0d6e
watercyb <watercyb@hotmail.com>
2026-05-08 22:29:18 -0700

    Add solution: 3614. Process String with Special Operations II

---
+++ 3614. Process String with Special Operations II/3614. Process String with Special Operations II.java
@@ -0,0 +1,44 @@
+/*
+ * Problem: 3614. Process String with Special Operations II
+ * Difficulty: Hard
+ * Link: https://leetcode.com/problems/process-string-with-special-operations-ii/
+ * Language: java
+ * Date: 2026-05-09
+ */
+
+class Solution {
+    public char processStr(String s, long k) {
+        char[] chrs = s.toCharArray();
+        long count = 0;
+        for (char chr : chrs) {
+            if (chr == '*') {
+                if (count > 0)
+                    count--;
+            } else if (chr == '#') {
+                count *= 2;
+            } else if (chr != '%') {
+                count++;
+            }
+        }
+        k++;
+        if (count < k)
+            return '.';
+        for (int i = chrs.length - 1; i >= 0; i--) {
+            if (chrs[i] == '*') {
+                count++;
+            } else if (chrs[i] == '#') {
+                long h = count / 2;
+                if (k > h)
+                    k -= h;
+                count /= 2;
+            } else if (chrs[i] == '%') {
+                k = count + 1 - k;
+            } else {
+                if (k == count)
+                    return chrs[i];
+                count--;
+            }
+        }
+        return '.';
+    }
+}
