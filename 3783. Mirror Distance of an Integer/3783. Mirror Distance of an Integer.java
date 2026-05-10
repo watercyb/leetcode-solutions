d14b454112824d8d988a986f7294234876e8e487
watercyb <watercyb@hotmail.com>
2026-05-09 22:40:42 -0700

    Update solution: 3783. Mirror Distance of an Integer

--- 3783. Mirror Distance of an Integer/3783. Mirror Distance of an Integer.java
+++ 3783. Mirror Distance of an Integer/3783. Mirror Distance of an Integer.java
@@ -1,9 +1,9 @@
 /*
  * Problem: 3783. Mirror Distance of an Integer
  * Difficulty: Easy
- * Link: https://leetcode.com/problems/mirror-distance-of-an-integer/?envType=daily-question&envId=2026-04-18
+ * Link: https://leetcode.com/problems/mirror-distance-of-an-integer/
  * Language: java
- * Date: 2026-04-18
+ * Date: 2026-05-10
  */
 
 class Solution {
