d5db9f77020ece064e22705f91500e28b4a4cf6d
watercyb <watercyb@hotmail.com>
2026-05-09 22:36:57 -0700

    Add solution: 3754. Concatenate Non-Zero Digits and Multiply by Sum I

---
+++ 3754. Concatenate Non-Zero Digits and Multiply by Sum I/3754. Concatenate Non-Zero Digits and Multiply by Sum I.java
@@ -0,0 +1,25 @@
+/*
+ * Problem: 3754. Concatenate Non-Zero Digits and Multiply by Sum I
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i/
+ * Language: java
+ * Date: 2026-05-10
+ */
+
+class Solution {
+    public long sumAndMultiply(int n) {
+        long num = 0;
+        int sum = 0;
+        int base = 1;
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit != 0) {
+                num += digit * base;
+                sum += digit;
+                base *= 10;
+            }
+            n /= 10;
+        }
+        return num * sum;
+    }
+}
