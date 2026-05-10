6f701f371987e780186f51c59e29039f9030e827
watercyb <watercyb@hotmail.com>
2026-05-09 22:34:56 -0700

    Add solution: 3740. Minimum Distance Between Three Equal Elements I

---
+++ 3740. Minimum Distance Between Three Equal Elements I/3740. Minimum Distance Between Three Equal Elements I.py
@@ -0,0 +1,11 @@
+#
+# Problem: 3740. Minimum Distance Between Three Equal Elements I
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/minimum-distance-between-three-equal-elements-i/
+# Language: python3
+# Date: 2026-05-10
+
+
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        
