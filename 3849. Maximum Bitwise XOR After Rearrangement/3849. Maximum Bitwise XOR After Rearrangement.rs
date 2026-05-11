4915667da22ca93df3e5c63d135f5d43cd6268e2
watercyb <watercyb@hotmail.com>
2026-05-10 23:15:00 -0700

    Add solution: 3849. Maximum Bitwise XOR After Rearrangement

---
+++ 3849. Maximum Bitwise XOR After Rearrangement/3849. Maximum Bitwise XOR After Rearrangement.rs
@@ -0,0 +1,28 @@
+/*
+ * Problem: 3849. Maximum Bitwise XOR After Rearrangement
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/maximum-bitwise-xor-after-rearrangement/
+ * Language: rust
+ * Date: 2026-05-11
+ */
+
+impl Solution {
+    pub fn maximum_xor(s: String, t: String) -> String {
+        let mut counts=[0;2];
+        for chr in t.chars() {
+            counts[(chr as u8-b'0')as usize]+=1;
+        }
+        let mut res=String::new();
+        for chr in s.chars() {
+            let idx=(chr as u8-b'0') as usize;
+            if counts[idx^1]>0 {
+                counts[idx^1]-=1;
+                res.push_str("1");
+            } else {
+                counts[idx]-=1;
+                res.push_str("0");
+            }
+        }
+        return res;
+    }
+}
