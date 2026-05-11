4eb341e665ecd719c17bc38084691e9403241b9b
watercyb <watercyb@hotmail.com>
2026-05-10 23:17:49 -0700

    Add solution: 3867. Sum of GCD of Formed Pairs

---
+++ 3867. Sum of GCD of Formed Pairs/3867. Sum of GCD of Formed Pairs.cs
@@ -0,0 +1,32 @@
+/*
+ * Problem: 3867. Sum of GCD of Formed Pairs
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/sum-of-gcd-of-formed-pairs/
+ * Language: csharp
+ * Date: 2026-05-11
+ */
+
+public class Solution {
+    public long GcdSum(int[] nums) {
+        int max=0;
+        for (int i=0;i<nums.Length;i++) {
+            max=Math.Max(max,nums[i]);
+            nums[i]=GCD(max, nums[i]);
+        }
+        int l=0;
+        int r=nums.Length-1;
+        long res=0;
+        Array.Sort(nums);
+        while (l<r) {
+            res+=GCD(nums[l],nums[r]);
+            l++;
+            r--;
+        }
+        return res;
+    }
+
+    public int GCD(int a, int b) {
+        if (b==0) return a;
+        return GCD(b,a%b);
+    }
+}
