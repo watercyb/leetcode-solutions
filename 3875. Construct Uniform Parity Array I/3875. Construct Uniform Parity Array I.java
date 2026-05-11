9a8f92657714f2178a7bed6c7fc40131d01867c5
watercyb <watercyb@hotmail.com>
2026-05-10 23:20:11 -0700

    Add solution: 3875. Construct Uniform Parity Array I

---
+++ 3875. Construct Uniform Parity Array I/3875. Construct Uniform Parity Array I.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 3875. Construct Uniform Parity Array I
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/construct-uniform-parity-array-i/
+ * Language: java
+ * Date: 2026-05-11
+ */
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
