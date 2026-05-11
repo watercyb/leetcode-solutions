75f4ea3898af99d1c48c0a0b79d2605489289388
watercyb <watercyb@hotmail.com>
2026-05-10 23:20:32 -0700

    Update solution: 3879. Maximum Distinct Path Sum in a Binary Tree

--- 3879. Maximum Distinct Path Sum in a Binary Tree/3879. Maximum Distinct Path Sum in a Binary Tree.java
+++ 3879. Maximum Distinct Path Sum in a Binary Tree/3879. Maximum Distinct Path Sum in a Binary Tree.java
@@ -3,7 +3,7 @@
  * Difficulty: Medium
  * Link: https://leetcode.com/problems/maximum-distinct-path-sum-in-a-binary-tree/
  * Language: java
- * Date: 2026-03-26
+ * Date: 2026-05-11
  */
 
 /**
@@ -22,73 +22,54 @@
  * }
  */
 class Solution {
-
-    int idx = 0;
-
     public int maxSum(TreeNode root) {
-        Map<Integer, List<Integer>> links = new HashMap<>();
-        Map<Integer, Integer> values = new HashMap<>();
-
-        dfsLinks(root, -1, links, values);
-
-        int res = -100000000;
-
-        for (int k : links.keySet()) {
-            Set<Integer> seen = new HashSet<>();
-            res = Math.max(res, dfs(links, values, seen, k, -1));
+        List<List<Integer>> lists = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+        dfsLinks(root, -1, lists, values);
+        int[][] links = new int[lists.size()][];
+        int[] vals = new int[values.size()];
+        for (int i = 0; i < links.length; i++) {
+            List<Integer> list = lists.get(i);
+            links[i] = new int[list.size()];
+            for (int j = 0; j < links[i].length; j++) {
+                links[i][j] = list.get(j);
+            }
+            vals[i] = values.get(i);
         }
-
+        int res = -100000000;
+        boolean[] seens = new boolean[2001];
+        for (int i = 0; i < links.length; i++) {
+            res = Math.max(res, dfs(links, vals, seens, i, -1));
+        }
         return res;
     }
 
-    public int dfsLinks(TreeNode node, int prv,
-            Map<Integer, List<Integer>> links,
-            Map<Integer, Integer> values) {
-
+    public int dfsLinks(TreeNode node, int prv, List<List<Integer>> links, List<Integer> values) {
+        int currentIdx = links.size();
         List<Integer> arr = new ArrayList<>();
-        int currentIdx = idx++;
-
-        if (prv != -1) {
+        links.add(arr);
+        values.add(node.val);
+        if (prv != -1)
             arr.add(prv);
-        }
-
-        if (node.left != null) {
+        if (node.left != null)
             arr.add(dfsLinks(node.left, currentIdx, links, values));
-        }
-
-        if (node.right != null) {
+        if (node.right != null)
             arr.add(dfsLinks(node.right, currentIdx, links, values));
-        }
-
-        links.put(currentIdx, arr);
-        values.put(currentIdx, node.val);
-
         return currentIdx;
     }
 
-    public int dfs(Map<Integer, List<Integer>> links,
-            Map<Integer, Integer> values,
-            Set<Integer> seen,
-            int i, int prv) {
-
-        int val = values.get(i);
-
-        if (seen.contains(val)) {
+    public int dfs(int[][] links, int[] values, boolean[] seens, int i, int prv) {
+        int val = values[i];
+        if (seens[val + 1000])
             return -100000000;
-        }
-
-        seen.add(val);
-
+        seens[val + 1000] = true;
         int res = 0;
-
-        for (int next : links.get(i)) {
+        for (int next : links[i]) {
             if (next != prv) {
-                res = Math.max(res, dfs(links, values, seen, next, i));
+                res = Math.max(res, dfs(links, values, seens, next, i));
             }
         }
-
-        seen.remove(val);
-
+        seens[val + 1000] = false;
         return Math.max(res, 0) + val;
     }
 }
