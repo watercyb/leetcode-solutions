cabd26b43b224b9601280b44dbdad485276b92a5
watercyb <watercyb@hotmail.com>
2026-05-10 23:28:00 -0700

    Update solution: 3911. K-th Smallest Remaining Even Integer in Subarray Queries

--- 3911. K-th Smallest Remaining Even Integer in Subarray Queries/3911. K-th Smallest Remaining Even Integer in Subarray Queries.java
+++ 3911. K-th Smallest Remaining Even Integer in Subarray Queries/3911. K-th Smallest Remaining Even Integer in Subarray Queries.java
@@ -3,7 +3,7 @@
  * Difficulty: Hard
  * Link: https://leetcode.com/problems/k-th-smallest-remaining-even-integer-in-subarray-queries/
  * Language: java
- * Date: 2026-04-28
+ * Date: 2026-05-11
  */
 
 class Solution {
