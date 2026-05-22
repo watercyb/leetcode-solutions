94609b35dbd0f4a0fb75563d920b9f4250d770a8
watercyb <watercyb@hotmail.com>
2026-05-21 17:13:48 -0700

    Update solution: 33. Search in Rotated Sorted Array

--- 33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.java
+++ 33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.java
@@ -1,54 +1,56 @@
 /*
  * Problem: 33. Search in Rotated Sorted Array
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/search-in-rotated-sorted-array/
+ * Link: https://leetcode.com/problems/search-in-rotated-sorted-array/?envType=daily-question&envId=2026-05-22
  * Language: java
- * Date: 2026-03-25
+ * Date: 2026-05-22
  */
 
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length - 1;
-        if (n < 0)
+        int l = 0;
+        int r = nums.length - 1;
+        if (nums[l] <= nums[r]) {
+            while (l < r) {
+                int mid = (l + r) >>> 1;
+                if (nums[mid] >= target) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            if (nums[l] == target)
+                return l;
             return -1;
-        int left = 0;
-        int right = n;
-        if (left == right) {
-            if (nums[left] == target) {
-                return left;
-            } else {
-                return -1;
+        } else if (nums[r] >= target) {
+            while (l < r) {
+                int mid = (l + r) >>> 1;
+                if (nums[mid] == target)
+                    return mid;
+                if (nums[mid] >= target && nums[mid] <= nums[nums.length - 1]) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
             }
+            if (nums[l] == target)
+                return l;
+            return -1;
+        } else {
+            while (l < r) {
+                int mid = (l + r) >>> 1;
+                if (nums[mid] == target)
+                    return mid;
+                if (nums[mid] >= target || nums[mid] < nums[0]) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            if (nums[l] == target)
+                return l;
+            return -1;
         }
-        int mid = 1;
 
-        if (nums[left] > nums[right]) {
-            while (left < right) {
-                mid = (right - left) / 2 + left;
-                
-                if (nums[mid] >= nums[0])
-                    left = mid + 1;
-                if (nums[mid] < nums[n])
-                    right = mid;
-
-            }
-            if (nums[n] >= target) {
-                right = n;
-            } else {
-                left = 0;
-                right--;
-            }
-        }
-        
-        while (left <= right) {
-            mid = (right - left) / 2 + left;
-            if (nums[mid] == target)
-                return mid;
-            if (nums[mid] < target)
-                left = mid + 1;
-            if (nums[mid] > target)
-                right = mid - 1;
-        }
-        return -1;
     }
 }
