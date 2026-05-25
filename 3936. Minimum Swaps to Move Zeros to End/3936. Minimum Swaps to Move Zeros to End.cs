5b18736bfb87d5d62d2a3a47d67a78070eab5700
watercyb <watercyb@hotmail.com>
2026-05-25 13:21:27 -0700

    Add solution: 3936. Minimum Swaps to Move Zeros to End

---
+++ 3936. Minimum Swaps to Move Zeros to End/3936. Minimum Swaps to Move Zeros to End.cs
@@ -0,0 +1,32 @@
+/*
+ * Problem: 3936. Minimum Swaps to Move Zeros to End
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/minimum-swaps-to-move-zeros-to-end/
+ * Language: csharp
+ * Date: 2026-05-25
+ */
+
+public class Solution {
+    public int MinimumSwaps(int[] nums) {
+        int l=0;
+        int r=nums.Length-1;
+        int res=0;
+        while (l<r) {
+            if (nums[l]!=0) 
+            {
+                l++;
+            } 
+            else if (nums[r]==0) 
+            {
+                r--;
+            }
+            else 
+            {
+                res++;
+                l++;
+                r--;
+            }
+        }
+        return res;
+    }
+}
