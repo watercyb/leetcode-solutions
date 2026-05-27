6f47ac2b7e547b1368c8ba13293b7158dd344a06
watercyb <watercyb@hotmail.com>
2026-05-27 16:24:22 -0700

    Update solution: 3944. Minimum Operations to Make Array Modulo Alternating II

--- 3944. Minimum Operations to Make Array Modulo Alternating II/3944. Minimum Operations to Make Array Modulo Alternating II.java
+++ 3944. Minimum Operations to Make Array Modulo Alternating II/3944. Minimum Operations to Make Array Modulo Alternating II.java
@@ -7,102 +7,89 @@
  */
 
 class Solution {
-
     public long minOperations(int[] nums, int k) {
         if (nums.length == 1)
             return 0;
         for (int i = 0; i < nums.length; i++) {
             nums[i] = nums[i] % k;
         }
-        long[] min1 = getMin(nums, k, 0);
-        long[] min2 = getMin(nums, k, 1);
-        if (min1[1] != min2[1])
-            return min1[0] + min2[0];
-        return Math.min(min1[0] + min2[2], min1[2] + min2[0]);
+        Min min1 = getMin(nums, k, 0);
+        Min min2 = getMin(nums, k, 1);
+        if (min1.num != min2.num)
+            return min1.min1 + min2.min1;
+        return Math.min(min1.min1 + min2.min2, min1.min2 + min2.min1);
     }
 
-    private long[] getMin(int[] nums, int k, int startIdx) {
+    public Min getMin(int[] nums, int k, int startIdx) {
+        Min res = new Min();
         int n = (nums.length - startIdx + 1) / 2;
-        int[] arr = new int[2 * n];
-        int idx = startIdx;
-        int l = 0;
-        int r = n;
-        while (idx < nums.length) {
-            arr[l] = nums[idx];
-            arr[r] = nums[idx] + k;
-            idx += 2;
-            l++;
-            r++;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = nums[startIdx];
+            startIdx += 2;
         }
         Arrays.sort(arr);
-        l = 0;
-        r = 0;
-        long sumL = 0;
-        long sumR = 0;
-        while (r < n) {
-            sumR += arr[r++];
+        long[] sums = new long[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            sums[i + 1] = sums[i] + arr[i];
         }
-        long sum = sumR - (long) arr[0] * n;
-        long min1 = sum;
-        long num1 = arr[0] % k;
-        long min2 = sum + n;
-        idx = 0;
-        while (r < arr.length) {
-            long min = (long) arr[idx] * (idx - l) - sumL + sumR - (long) arr[idx] * (r - idx);
-            if (min < min1) {
-                if (arr[idx] % k != num1)
-                    min2 = min1;
-                min1 = min;
-                num1 = arr[idx] % k;
-            } else if (min < min2 && num1 != arr[idx] % k) {
-                min2 = min;
-            }
-            if (idx > 0 && arr[idx] % k - arr[idx - 1] % k > 1) {
-                long minL = min - (idx - l) + (r - idx);
-                min2 = Math.min(min2, minL);
-            }
-            if (idx < arr.length - 1 && arr[idx + 1] % k - arr[idx] % k > 1) {
-                long minR = min + (idx - l) - (r - idx) + 2;
-                min2 = Math.min(min2, minR);
-            }
-            while (idx < r - 1) {
-                long minNext = (long) arr[idx + 1] * (idx + 1 - l) - (sumL + arr[idx]) + (sumR - arr[idx])
-                        - (long) arr[idx + 1] * (r - idx - 1);
-                if (minNext <= min) {
-                    sumL += arr[idx];
-                    sumR -= arr[idx];
-                    idx++;
-                    if (minNext < min1) {
-                        if (arr[idx] % k != num1)
-                            min2 = min1;
-                        min1 = minNext;
-                        num1 = arr[idx] % k;
-                    } else if (minNext < min2 && num1 != arr[idx] % k) {
-                        min2 = minNext;
-                    }
-                    if (idx > 0 && arr[idx] % k - arr[idx - 1] % k > 1) {
-                        long minL = minNext - (idx - l) + (r - idx);
-                        min2 = Math.min(min2, minL);
-                    }
-                    if (idx < arr.length - 1 && arr[idx + 1] % k - arr[idx] % k > 1) {
-                        long minR = minNext + (idx - l) - (r - idx) + 2;
-                        min2 = Math.min(min2, minR);
-                    }
-                    min = minNext;
-                } else {
-                    break;
-                }
-            }
-            sumL -= arr[l];
-            sumR += arr[r];
-            if (idx == l) {
-                sumL += arr[idx];
-                sumR -= arr[idx];
+        int l = 0;
+        int r = arr.length - 1;
+        int idx = 0;
+        while (arr[r] - arr[0] > k - arr[r] + arr[0]) {
+            r--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int target = arr[i];
+            if (i > 0 && target == arr[i - 1])
+                continue;
+            while (arr[idx] < target) {
                 idx++;
             }
-            l++;
-            r++;
+            while (target - arr[l] > k - target + arr[l]) {
+                l++;
+            }
+            while (r < arr.length - 1 && arr[r + 1] - target <= k - arr[r + 1] + target) {
+                r++;
+            }
+            long sum = 0;
+            if (l > 0) {
+                sum += (long) k * l - ((long) target * l - (sums[l] - sums[0]));
+            } else if (r < arr.length - 1) {
+                sum += (long) k * (arr.length - 1 - r)
+                        - (sums[arr.length] - sums[r + 1] - (long) target * (arr.length - 1 - r));
+            }
+            sum += (long) target * (idx - l) - (sums[idx] - sums[l]) + (sums[r + 1] - sums[idx])
+                    - (long) target * (r - idx + 1);
+            res.updateMin(sum, target);
         }
-        return new long[] { min1, num1, min2 };
+        getSum(arr, res, k, (res.num - 1 + k) % k);
+        getSum(arr, res, k, (res.num + 1) % k);
+        return res;
+    }
+
+    public void getSum(int[] arr, Min min, int k, int target) {
+        int sum = 0;
+        for (int num : arr) {
+            int diff = Math.abs(target - num);
+            sum += Math.min(diff, k - diff);
+        }
+        min.updateMin(sum, target);
+    }
+}
+
+class Min {
+    long min1 = Long.MAX_VALUE;
+    int num = -1;
+    long min2 = Long.MAX_VALUE;
+
+    public void updateMin(long min, int num) {
+        if (min < min1) {
+            min2 = min1;
+            min1 = min;
+            this.num = num;
+        } else if (min < min2) {
+            min2 = min;
+        }
     }
 }
