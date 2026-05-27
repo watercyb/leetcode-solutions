3ce79b558612e457eadcbd9042ff89570bbabacf
watercyb <watercyb@hotmail.com>
2026-05-26 21:22:49 -0700

    Update solution: Unknown Problem

--- Unknown Problem/Unknown Problem.kt
+++ Unknown Problem/Unknown Problem.kt
@@ -1,13 +1,13 @@
 /*
  * Problem: Unknown Problem
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/minimum-operations-to-make-array-modulo-alternating-i/
+ * Link: https://leetcode.com/problems/minimum-operations-to-make-array-modulo-alternating-ii/
  * Language: kotlin
  * Date: 2026-05-27
  */
 
 class Solution {
-    fun minOperations(nums: IntArray, k: Int): Int {
+    fun minOperations(nums: IntArray, k: Int): Long {
         if (nums.size==1) return 0
         for (i in 0 until nums.size) {
             nums[i]=nums[i]%k
@@ -18,7 +18,7 @@ class Solution {
         return minOf(min1[0]+min2[2], min1[2]+min2[0])
     }
 
-    fun getMin(nums: IntArray, k:Int, idx:Int): IntArray {
+    fun getMin(nums: IntArray, k:Int, idx:Int): LongArray {
         val n=(nums.size-idx+1)/2
         val arr=IntArray(2*n)
         var idx=idx
@@ -34,18 +34,18 @@ class Solution {
         arr.sort()
         l=0
         r=0
-        var sumL=0
-        var sumR=0
+        var sumL: Long=0
+        var sumR: Long=0
         while (r<n) {
             sumR+=arr[r++]
         }
-        var sum=sumR-arr[0]*n
-        var min1=sum
+        var sum:Long=sumR-arr[0].toLong()*n
+        var min1: Long=sum
         var num1=arr[0]%k
         var min2=sum+n
         idx=0
         while (r<arr.size) {
-            var min=arr[idx]*(idx-l)-sumL+sumR-arr[idx]*(r-idx)
+            var min: Long=arr[idx].toLong()*(idx-l)-sumL+sumR-arr[idx].toLong()*(r-idx)
             if (min<min1) {
                 if (arr[idx]%k!=num1)
                     min2=min1
@@ -63,7 +63,7 @@ class Solution {
                 if (minR<min2) min2=minR
             }
             while (idx<r) {
-                var minNext=arr[idx+1]*(idx+1-l)-(sumL+arr[idx])+(sumR-arr[idx])-arr[idx+1]*(r-idx-1)
+                var minNext: Long=arr[idx+1].toLong()*(idx+1-l)-(sumL+arr[idx])+(sumR-arr[idx])-arr[idx+1].toLong()*(r-idx-1)
                 if (minNext<=min) {
                     sumL+=arr[idx]
                     sumR-=arr[idx]
@@ -89,7 +89,7 @@ class Solution {
                     break;
                 }
             }
-            if (idx < arr.size - 1 && arr[idx + 1] % k != arr[idx] % k && (arr[idx] + 1) % k != min1)
+            if (idx < arr.size - 1 && arr[idx + 1] % k != arr[idx] % k && (arr[idx] + 1) % k != num1)
                 min2 = Math.min(min2, min + (idx - l) - (r - idx) + 2);
             sumL-=arr[l]
             sumR+=arr[r]
@@ -101,6 +101,6 @@ class Solution {
             l++
             r++                
         }
-        return intArrayOf(min1, num1, min2)
+        return longArrayOf(min1, num1.toLong(), min2)
     }
 }
