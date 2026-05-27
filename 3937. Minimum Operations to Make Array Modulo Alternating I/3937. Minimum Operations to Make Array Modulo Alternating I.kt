1c2d33e2c6f6a3330b11f76f530acc1eb618bdb4
watercyb <watercyb@hotmail.com>
2026-05-26 21:23:25 -0700

    Update solution: 3937. Minimum Operations to Make Array Modulo Alternating I

--- 3937. Minimum Operations to Make Array Modulo Alternating I/3937. Minimum Operations to Make Array Modulo Alternating I.kt
+++ 3937. Minimum Operations to Make Array Modulo Alternating I/3937. Minimum Operations to Make Array Modulo Alternating I.kt
@@ -3,31 +3,104 @@
  * Difficulty: Medium
  * Link: https://leetcode.com/problems/minimum-operations-to-make-array-modulo-alternating-i/
  * Language: kotlin
- * Date: 2026-05-26
+ * Date: 2026-05-27
  */
 
 class Solution {
     fun minOperations(nums: IntArray, k: Int): Int {
-        val dp = Array(2) { IntArray(k) }
+        if (nums.size==1) return 0
         for (i in 0 until nums.size) {
-            for (j in 0 until k) {
-                val diff=Math.abs(nums[i]%k-j);
-                dp[(i and 1)][j]+=Math.min(diff, k-diff)
+            nums[i]=nums[i]%k
+        }
+        val min1=getMin(nums, k, 0)
+        val min2=getMin(nums, k, 1)
+        if (min1[1]!=min2[1]) return min1[0]+min2[0]
+        return minOf(min1[0]+min2[2], min1[2]+min2[0])
+    }
+
+    fun getMin(nums: IntArray, k:Int, idx:Int): IntArray {
+        val n=(nums.size-idx+1)/2
+        val arr=IntArray(2*n)
+        var idx=idx
+        var l=0
+        var r=n
+        while (idx<nums.size) {
+            arr[l]=nums[idx]
+            arr[r]=nums[idx]+k
+            idx+=2
+            l++
+            r++
+        }
+        arr.sort()
+        l=0
+        r=0
+        var sumL=0
+        var sumR=0
+        while (r<n) {
+            sumR+=arr[r++]
+        }
+        var sum=sumR-arr[0]*n
+        var min1=sum
+        var num1=arr[0]%k
+        var min2=sum+n
+        idx=0
+        while (r<arr.size) {
+            var min=arr[idx]*(idx-l)-sumL+sumR-arr[idx]*(r-idx)
+            if (min<min1) {
+                if (arr[idx]%k!=num1)
+                    min2=min1
+                min1=min
+                num1=arr[idx]%k
+            } else if (min<min2&&num1!=arr[idx]%k) {
+                min2=min
             }
+            if (idx>0&&arr[idx]%k-arr[idx-1]%k>1) {
+                val minL=min-(idx-l)+(r-idx)
+                if (minL<min2) min2=minL
+            }
+            if (idx<arr.size-1&&arr[idx+1]%k-arr[idx]%k>1) {
+                val minR=min+(idx-l)-(r-idx)+2
+                if (minR<min2) min2=minR
+            }
+            while (idx<r) {
+                var minNext=arr[idx+1]*(idx+1-l)-(sumL+arr[idx])+(sumR-arr[idx])-arr[idx+1]*(r-idx-1)
+                if (minNext<=min) {
+                    sumL+=arr[idx]
+                    sumR-=arr[idx]
+                    idx++
+                    if (minNext<min1) {
+                        if (arr[idx]%k!=num1)
+                            min2=min1
+                        min1=minNext
+                        num1=arr[idx]%k
+                    } else if (minNext<min2&&num1!=arr[idx]%k) {
+                        min2=minNext
+                    }
+                    if (arr[idx]%k-arr[idx-1]%k>1) {
+                        val minL=minNext-(idx-l)+(r-idx)
+                        if (minL<min2) min2=minL
+                    }
+                    if (idx<arr.size-1&&arr[idx+1]%k-arr[idx]%k>1) {
+                        val minR=minNext+(idx-l)-(r-idx)+2
+                        if (minR<min2) min2=minR
+                    }
+                    min=minNext;
+                } else {
+                    break;
+                }
+            }
+            if (idx < arr.size - 1 && arr[idx + 1] % k != arr[idx] % k && (arr[idx] + 1) % k != num1)
+                min2 = Math.min(min2, min + (idx - l) - (r - idx) + 2);
+            sumL-=arr[l]
+            sumR+=arr[r]
+            if (idx==l) {
+                sumL+=arr[idx]
+                sumR-=arr[idx]
+                idx++
+            }
+            l++
+            r++                
         }
-        var mins=IntArray(k)
-        var min=Integer.MAX_VALUE
-        for (i in 0 until k) {
-            mins[i]=min
-            min=Math.min(min, dp[0][i])
-        }
-        min=Integer.MAX_VALUE
-        var res=Integer.MAX_VALUE
-        for (i in k-1 downTo 0) {
-            mins[i]=Math.min(mins[i], min)
-            min=Math.min(min, dp[0][i])
-            res=Math.min(res, mins[i]+dp[1][i])
-        }
-        return res
+        return intArrayOf(min1, num1, min2)
     }
 }
