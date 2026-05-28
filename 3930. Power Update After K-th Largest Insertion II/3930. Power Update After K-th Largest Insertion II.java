1c383ea89bdf53aa79d817582da20bff90642b66
watercyb <watercyb@hotmail.com>
2026-05-27 21:50:22 -0700

    Update solution: 3930. Power Update After K-th Largest Insertion II

--- 3930. Power Update After K-th Largest Insertion II/3930. Power Update After K-th Largest Insertion II.java
+++ 3930. Power Update After K-th Largest Insertion II/3930. Power Update After K-th Largest Insertion II.java
@@ -3,65 +3,55 @@
  * Difficulty: Hard
  * Link: https://leetcode.com/problems/power-update-after-k-th-largest-insertion-ii/
  * Language: java
- * Date: 2026-05-13
+ * Date: 2026-05-28
  */
 
 class Solution {
-    int mod = 1_000_000_007;
-
     public List<Integer> powerUpdate(int[] nums, int p, int[][] queries) {
-        Node root = new Node(-1, 0);
-        for (int num : nums) {
-            insert(root, 0, 1000000000, num);
+        long[] arr = new long[nums.length + queries.length];
+        BIT = new int[arr.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = ((long) nums[i] << 32);
+        }
+        for (int i = 0; i < queries.length; i++) {
+            arr[nums.length + i] = ((long) queries[i][0] << 32) + i + 1;
+        }
+        Arrays.sort(arr);
+        int l = 0;
+        int r = arr.length - 1;
+        while (l < r) {
+            long temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+        int[] indexes = new int[queries.length];
+        for (int i = 0; i < arr.length; i++) {
+            int idx = (int) arr[i];
+            if (idx != 0) {
+                indexes[idx - 1] = i;
+            } else {
+                insert(i);
+            }
         }
         List<Integer> res = new ArrayList<>();
-        for (int[] query : queries) {
-            insert(root, 0, 1000000000, query[0]);
-            p = pow(p, get(root, 0, 1000000000, root.count - query[1] + 1));
+        int n = 31 - Integer.numberOfLeadingZeros(arr.length);
+        for (int i = 0; i < queries.length; i++) {
+            insert(indexes[i]);
+            int idx = 0;
+            int k = queries[i][1];
+            for (int j = n; j >= 0; j--) {
+                if ((idx + (1 << j)) < BIT.length && BIT[idx + (1 << j)] < k) {
+                    idx += 1 << j;
+                    k -= BIT[idx];
+                }
+            }
+            int num = (int) (arr[idx] >> 32);
+            p = pow(p, num);
             res.add(p);
         }
         return res;
-    }
-
-    public void insert(Node node, int l, int r, int val) {
-        if (l == r || node.val == val) {
-            node.count++;
-            return;
-        }
-        int mid = (l + r) >>> 1;
-        if (node.val >= 0) {
-            if (mid >= node.val) {
-                node.left = new Node(node.val, node.count);
-            } else {
-                node.right = new Node(node.val, node.count);
-            }
-            node.val = -1;
-        }
-        node.count++;
-        if (mid >= val) {
-            if (node.left == null) {
-                node.left = new Node(val, 1);
-            } else {
-                insert(node.left, l, mid, val);
-            }
-        } else {
-            if (node.right == null) {
-                node.right = new Node(val, 1);
-            } else {
-                insert(node.right, mid + 1, r, val);
-            }
-        }
-    }
-
-    public int get(Node node, int l, int r, int sum) {
-        if (l == r || node.val >= 0)
-            return node.val;
-        int mid = (l + r) >>> 1;
-        if (node.left != null && sum <= node.left.count) {
-            return get(node.left, l, mid, sum);
-        } else {
-            return get(node.right, mid + 1, r, sum - (node.left == null ? 0 : node.left.count));
-        }
     }
 
     public int pow(long a, int b) {
@@ -74,16 +64,25 @@ class Solution {
         }
         return (int) res;
     }
-}
 
-class Node {
-    Node left;
-    Node right;
-    int val;
-    int count;
+    int[] BIT;
+    int mod = 1_000_000_007;
 
-    public Node(int val, int count) {
-        this.val = val;
-        this.count = count;
+    public void insert(int i) {
+        i++;
+        while (i < BIT.length) {
+            BIT[i]++;
+            i += i & -i;
+        }
+    }
+
+    public int get(int i) {
+        i++;
+        int res = 0;
+        while (i > 0) {
+            res += BIT[i];
+            i -= i & -i;
+        }
+        return res;
     }
 }
