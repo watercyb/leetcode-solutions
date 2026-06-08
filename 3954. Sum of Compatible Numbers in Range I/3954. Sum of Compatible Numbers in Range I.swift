5bb1ab600dcb65dc7d41148dd3c6d2935c65cc85
watercyb <watercyb@hotmail.com>
2026-06-08 13:21:28 -0700

    Add solution: 3954. Sum of Compatible Numbers in Range I

---
+++ 3954. Sum of Compatible Numbers in Range I/3954. Sum of Compatible Numbers in Range I.swift
@@ -0,0 +1,19 @@
+/*
+ * Problem: 3954. Sum of Compatible Numbers in Range I
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/sum-of-compatible-numbers-in-range-i/
+ * Language: swift
+ * Date: 2026-06-08
+ */
+
+class Solution {
+    func sumOfGoodIntegers(_ n: Int, _ k: Int) -> Int {
+        var res=0
+        for i in max(n-k,0)...n+k {
+            if n&i==0 {
+                res+=i
+            }
+        }
+        return res
+    }
+}
