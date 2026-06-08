3fe807ba3af8b6f562bdc34931be1c26c6869ef6
watercyb <watercyb@hotmail.com>
2026-06-08 13:22:22 -0700

    Add solution: Unknown Problem

---
+++ Unknown Problem/Unknown Problem.swift
@@ -0,0 +1,19 @@
+/*
+ * Problem: Unknown Problem
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/sum-of-compatible-numbers-in-range-i/
+ * Language: swift
+ * Date: 2026-06-08
+ */
+
+class Solution {
+    func sumOfGoodIntegers(_ n: Int, _ k: Int) -> Int {
+        var res=0
+        for i in max(n-k,1)...n+k {
+            if n&i==0 {
+                res+=i
+            }
+        }
+        return res
+    }
+}
