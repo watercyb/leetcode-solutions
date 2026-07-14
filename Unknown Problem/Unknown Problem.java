f56585f698e4d257d52a7c3395715f6d684c5711
watercyb <watercyb@hotmail.com>
2026-07-13 22:09:42 -0700

    Update solution: Unknown Problem

--- Unknown Problem/Unknown Problem.java
+++ Unknown Problem/Unknown Problem.java
@@ -1,54 +1,150 @@
 /*
  * Problem: Unknown Problem
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/maximum-total-value/
+ * Link: https://leetcode.com/problems/distinct-gate-paths-to-lca/
  * Language: java
- * Date: 2026-06-24
+ * Date: 2026-07-14
  */
 
 class Solution {
-    public int maxTotalValue(int[] value, int[] decay, int m) {
-        long total = m;
-        long sum = 0;
-        int max = 0;
-        for (int i = 0; i < value.length; i++) {
-            int count = Math.max((value[i] + decay[i] - 1) / decay[i], 0);
-            sum += (long) (value[i] - (long) decay[i] * (count - 1) + value[i]) * count / 2;
-            total -= count;
-            max = Math.max(max, value[i]);
+    public int distinctPaths(int n, int[] parent, int[][] gates, int[][] queries) {
+        boolean[] hasChilds = new boolean[n];
+        for (int p : parent) {
+            if (p == -1)
+                continue;
+            hasChilds[p] = true;
         }
-        if (total >= 0)
-            return (int) (sum % mod);
-        int l = 0;
-        int r = max + 1;
-        while (l < r) {
-            int mid = (l + r) >>> 1;
-            if (!chk(value, decay, m, mid)) {
-                r = mid;
-            } else {
-                l = mid + 1;
+        depths = new int[n];
+        Arrays.fill(depths, -1);
+        int max = getDepth(parent);
+        for (int depth : depths) {
+            max = Math.max(max, depth);
+        }
+        len = 32 - Integer.numberOfLeadingZeros(max);
+        BLT = new int[len][n];
+        m1 = new long[len][n];
+        m2 = new long[len][n];
+        m3 = new long[len][n];
+        m4 = new long[len][n];
+        for (int i = 0; i < n; i++) {
+            BLT[0][i] = parent[i];
+            getM(gates[i], i);
+        }
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < n; j++) {
+                int next = BLT[i - 1][j];
+                if (next == -1) {
+                    BLT[i][j] = -1;
+                } else {
+                    BLT[i][j] = BLT[i - 1][next];
+                    pro(i, j, next);
+                }
             }
         }
-        return getSum(value, decay, m, l - 1);
+        int res = 0;
+        for (int[] query : queries) {
+            res ^= getWays(query);
+        }
+        return res;
+    }
+
+    int[][] BLT;
+    long[][] m1;
+    long[][] m2;
+    long[][] m3;
+    long[][] m4;
+    int[] depths;
+    int len;
+
+    public int getWays(int[] query) {
+        int aNode = query[0];
+        long[] aArr = getArr(query[1]);
+        int bNode = query[2];
+        long[] bArr = getArr(query[3]);
+        int l = depths[aNode];
+        int r = depths[bNode];
+        if (l != r) {
+            if (l < r) {
+                for (int i = len - 1; i >= 0 && r > l; i--) {
+                    if (r - (1 << i) >= l) {
+                        bArr = proArr(bArr, i, bNode);
+                        bNode = BLT[i][bNode];
+                        r -= 1 << i;
+                    }
+                }
+            } else {
+                for (int i = len - 1; i >= 0 && l > r; i--) {
+                    if (l - (1 << i) >= r) {
+                        aArr = proArr(aArr, i, aNode);
+                        aNode = BLT[i][aNode];
+                        l -= 1 << i;
+                    }
+                }
+            }
+        }
+        if (aNode != bNode) {
+            for (int i = len - 1; i >= 0; i--) {
+                int aNext = BLT[i][aNode];
+                int bNext = BLT[i][bNode];
+                if (aNext != bNext) {
+                    aArr = proArr(aArr, i, aNode);
+                    bArr = proArr(bArr, i, bNode);
+                    aNode = aNext;
+                    bNode = bNext;
+                }
+            }
+            aArr = proArr(aArr, 0, aNode);
+            bArr = proArr(bArr, 0, bNode);
+        }
+        long aSum = (aArr[0] + aArr[1]) % mod;
+        long bSum = (bArr[0] + bArr[1]) % mod;
+        int pro = (int) (aSum * bSum % mod);
+        return pro;
+    }
+
+    public int getDepth(int[] parent) {
+        int max = 0;
+        for (int i = 0; i < parent.length; i++) {
+            max = Math.max(max, dfs(parent, i));
+        }
+        return max;
+    }
+
+    public int dfs(int[] parent, int i) {
+        if (depths[i] != -1)
+            return depths[i] + 1;
+        if (parent[i] == -1) {
+            depths[i] = 0;
+        } else {
+            depths[i] = dfs(parent, parent[i]);
+        }
+        return depths[i] + 1;
     }
 
     int mod = 1_000_000_007;
 
-    public int getSum(int[] value, int[] decay, int m, int n) {
-        long sum = 0;
-        for (int i = 0; i < value.length; i++) {
-            int count = Math.max((value[i] - n + decay[i] - 1) / decay[i], 0);
-            sum += (long) (value[i] - decay[i] * (count - 1) + value[i]) * count / 2;
-            m -= count;
-        }
-        return (int) ((sum - (long) (-m) * n) % mod);
+    public void getM(int[] gate, int i) {
+        m1[0][i] = gate[0];
+        m2[0][i] = gate[2];
+        m3[0][i] = gate[2];
+        m4[0][i] = gate[1];
     }
 
-    public boolean chk(int[] value, int[] decay, int m, int mid) {
-        mid--;
-        for (int i = 0; i < value.length && m > 0; i++) {
-            m -= Math.max((value[i] - mid + decay[i] - 1) / decay[i], 0);
-        }
-        return m <= 0;
+    public long[] getArr(int a) {
+        long[] res = new long[2];
+        res[1 - a] = 1;
+        return res;
+    }
+
+    public void pro(int i, int j, int next) {
+        m1[i][j] = (m1[i - 1][j] * m1[i - 1][next] + m2[i - 1][j] * m3[i - 1][next]) % mod;
+        m2[i][j] = (m1[i - 1][j] * m2[i - 1][next] + m2[i - 1][j] * m4[i - 1][next]) % mod;
+        m3[i][j] = (m3[i - 1][j] * m1[i - 1][next] + m4[i - 1][j] * m3[i - 1][next]) % mod;
+        m4[i][j] = (m3[i - 1][j] * m2[i - 1][next] + m4[i - 1][j] * m4[i - 1][next]) % mod;
+    }
+
+    public long[] proArr(long[] a, int i, int j) {
+        return new long[] { (a[0] * m1[i][j] + a[1] * m3[i][j]) % mod,
+                (a[0] * m2[i][j] + a[1] * m4[i][j]) % mod };
     }
 }
