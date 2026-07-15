308d73384f78eb5b85c8e125c9de90cd7e0faa1f
watercyb <watercyb@hotmail.com>
2026-07-14 20:44:30 -0700

    Update solution: 3658. GCD of Odd and Even Sums

--- 3658. GCD of Odd and Even Sums/3658. GCD of Odd and Even Sums.java
+++ 3658. GCD of Odd and Even Sums/3658. GCD of Odd and Even Sums.java
@@ -1,9 +1,9 @@
 /*
  * Problem: 3658. GCD of Odd and Even Sums
  * Difficulty: Easy
- * Link: https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+ * Link: https://leetcode.com/problems/gcd-of-odd-and-even-sums/?envType=daily-question&envId=2026-07-15
  * Language: java
- * Date: 2026-05-09
+ * Date: 2026-07-15
  */
 
 class Solution {
