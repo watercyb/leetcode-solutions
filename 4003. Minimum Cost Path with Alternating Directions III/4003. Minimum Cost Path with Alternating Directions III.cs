ebdcde753cd5be251a933fb4edbd7e29f16a1fe8
watercyb <watercyb@hotmail.com>
2026-07-27 16:58:03 -0700

    Add solution: 4003. Minimum Cost Path with Alternating Directions III

---
+++ 4003. Minimum Cost Path with Alternating Directions III/4003. Minimum Cost Path with Alternating Directions III.cs
@@ -0,0 +1,13 @@
+/*
+ * Problem: 4003. Minimum Cost Path with Alternating Directions III
+ * Difficulty: Hard
+ * Link: https://leetcode.com/problems/minimum-cost-path-with-alternating-directions-iii/
+ * Language: csharp
+ * Date: 2026-07-27
+ */
+
+public class Solution {
+    public long MinCost(int m, int n, int[][] penalty) {
+        
+    }
+}
