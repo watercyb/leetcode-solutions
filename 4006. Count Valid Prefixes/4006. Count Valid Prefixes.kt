a62e6ac0ebe7c147cfa7fd7da008dba057dac871
watercyb <watercyb@hotmail.com>
2026-08-05 14:13:14 -0700

    Add solution: 4006. Count Valid Prefixes

---
+++ 4006. Count Valid Prefixes/4006. Count Valid Prefixes.kt
@@ -0,0 +1,23 @@
+/*
+ * Problem: 4006. Count Valid Prefixes
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/count-valid-prefixes/
+ * Language: kotlin
+ * Date: 2026-08-05
+ */
+
+class Solution {
+    fun countValidPrefixes(s: String): Int {
+        var count=0
+        var res=0
+        for (chr in s) {
+            if (chr=='0') {
+                count++
+            } else {
+                count--
+            }
+            if (count>=-1&&count<=1) res++
+        }
+        return res
+    }
+}
