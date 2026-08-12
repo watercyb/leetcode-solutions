f43c49e9ba6051e45b1f172f9638b44a8fc7e32a
watercyb <watercyb@hotmail.com>
2026-08-11 19:49:38 -0700

    Update solution: 2958. Length of Longest Subarray With at Most K Frequency

--- 2958. Length of Longest Subarray With at Most K Frequency/2958. Length of Longest Subarray With at Most K Frequency.java
+++ 2958. Length of Longest Subarray With at Most K Frequency/2958. Length of Longest Subarray With at Most K Frequency.java
@@ -1,34 +1,33 @@
 /*
  * Problem: 2958. Length of Longest Subarray With at Most K Frequency
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/
+ * Link: https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/?envType=daily-question&envId=2026-08-12
  * Language: java
- * Date: 2026-05-02
+ * Date: 2026-08-12
  */
 
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
         if (k == nums.length)
             return nums.length;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (int num : nums) {
-            min = Math.min(min, num);
-            max = Math.max(max, num);
-        }
-        int[] counts = new int[max - min + 1];
+        HashMap<Integer, Integer> HM = new HashMap<>();
         int res = 0;
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (counts[nums[i] - min] == k) {
-                res = Math.max(i - j, res);
-                while (nums[j] != nums[i]) {
-                    counts[nums[j] - min]--;
+            if (HM.containsKey(nums[i])) {
+                int tmp = HM.get(nums[i]);
+                if (tmp == k) {
+                    res = Math.max(i - j, res);
+                    while (nums[j] != nums[i]) {
+                        HM.put(nums[j], HM.get(nums[j]) - 1);
+                        j++;
+                    }
                     j++;
+                } else {
+                    HM.put(nums[i], tmp + 1);
                 }
-                j++;
             } else {
-                counts[nums[i] - min]++;
+                HM.put(nums[i], 1);
             }
         }
         return Math.max(nums.length - j, res);
