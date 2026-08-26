51e342edc71caff89dac1a1449215e0abe404fbf
watercyb <watercyb@hotmail.com>
2026-08-26 14:32:52 -0700

    Add solution: 3945. Digit Frequency Score

---
+++ 3945. Digit Frequency Score/3945. Digit Frequency Score.cs
@@ -0,0 +1,22 @@
+/*
+ * Problem: 3945. Digit Frequency Score
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/digit-frequency-score/
+ * Language: csharp
+ * Date: 2026-08-26
+ */
+
+public class Solution {
+    public int DigitFrequencyScore(int n) {
+        int[] counts=new int[10];
+        while (n>0) {
+            counts[n%10]++;
+            n/=10;
+        }
+        int res=0;
+        for (int i=1;i<=9;i++) {
+            res+=i*counts[i];
+        }
+        return res;
+    }
+}
