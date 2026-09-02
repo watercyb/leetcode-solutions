7df22d33ed485b2595a1a4fad6f19552ad9bde93
watercyb <watercyb@hotmail.com>
2026-09-01 20:21:56 -0700

    Update solution: 3875. Construct Uniform Parity Array I

--- 3875. Construct Uniform Parity Array I/3875. Construct Uniform Parity Array I.java
+++ 3875. Construct Uniform Parity Array I/3875. Construct Uniform Parity Array I.java
@@ -1,9 +1,9 @@
 /*
  * Problem: 3875. Construct Uniform Parity Array I
  * Difficulty: Easy
- * Link: https://leetcode.com/problems/construct-uniform-parity-array-i/
+ * Link: https://leetcode.com/problems/construct-uniform-parity-array-i/?envType=daily-question&envId=2026-09-02
  * Language: java
- * Date: 2026-05-11
+ * Date: 2026-09-02
  */
 
 class Solution {
