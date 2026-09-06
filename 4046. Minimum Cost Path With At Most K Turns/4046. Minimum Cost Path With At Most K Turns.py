1ad288c860fd03b306776a81f54c503a3810b317
watercyb <watercyb@hotmail.com>
2026-09-06 12:59:03 -0700

    Add solution: 4046. Minimum Cost Path With At Most K Turns

---
+++ 4046. Minimum Cost Path With At Most K Turns/4046. Minimum Cost Path With At Most K Turns.py
@@ -0,0 +1,43 @@
+#
+# Problem: 4046. Minimum Cost Path With At Most K Turns
+# Difficulty: Hard
+# Link: https://leetcode.com/problems/minimum-cost-path-with-at-most-k-turns/
+# Language: python3
+# Date: 2026-09-06
+
+
+class Solution:
+    def minCost(self, grid: list[list[int]], k: int) -> int:
+        DIRS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        m, n = len(grid), len(grid[0])
+        INF = 10**18
+        cost = [
+            [[[INF] * (k + 1) for _ in range(4)] for _ in range(n)] for _ in range(m)
+        ]
+        steps = [[[INF] * 4 for _ in range(n)] for _ in range(m)]
+
+        pq = []
+        for d in range(4):
+            cost[0][0][d][0] = grid[0][0]
+            heapq.heappush(pq, (grid[0][0], 0, 0, d, 0))
+
+        while pq:
+            c, x, y, d, t = heapq.heappop(pq)
+            if c != cost[x][y][d][t] or steps[x][y][d] <= t:
+                continue
+            steps[x][y][d] = t
+            if x == m - 1 and y == n - 1:
+                return c
+
+            for nd, (dx, dy) in enumerate(DIRS):
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n:
+                    nt = t + (nd != d)
+                    if nt <= k:
+                        nc = c + grid[nx][ny]
+                        if nc < cost[nx][ny][nd][nt]:
+                            cost[nx][ny][nd][nt] = nc
+                            heapq.heappush(pq, (nc, nx, ny, nd, nt))
+
+        return -1
+
