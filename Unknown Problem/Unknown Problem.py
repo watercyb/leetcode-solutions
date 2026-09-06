16cf1857e6fb1ea0e38ba50170bdbbef60ee28bc
watercyb <watercyb@hotmail.com>
2026-09-06 13:05:58 -0700

    Update solution: Unknown Problem

--- Unknown Problem/Unknown Problem.py
+++ Unknown Problem/Unknown Problem.py
@@ -1,56 +1,43 @@
 #
 # Problem: Unknown Problem
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/minimum-operations-to-form-subset-sum-ii/
+# Link: https://leetcode.com/problems/minimum-cost-path-with-at-most-k-turns/
 # Language: python3
-# Date: 2026-08-31
+# Date: 2026-09-06
 
 
 class Solution:
-    def minOperations(self, nums: list[int], sum: int) -> int:
-        if sum in nums:
-            return 0
-        lim = (1 << (sum + 1)) - 1
-        dp = {0: 1}
-        lists = {}
-        for num in nums:
-            if num not in lists:
-                li = []
-                stp = 0
-                while num > 0:
-                    n = num
-                    s = stp
-                    while n <= sum:
-                        li.append([n, s])
-                        n <<= 1
-                        s += 1
-                    while (num & 1) == 0:
-                        num >>= 1
-                        stp += 1
-                        li.append([num, stp])
-                    num >>= 1
-                    stp += 1
-                lists[num] = li
-            dp_next = dp.copy()
-            for k, v in dp.items():
-                for [n, s] in lists[num]:
-                    new_stp = k + s
-                    new_vals = (v << n) & lim
-                    dp_next[new_stp] = dp_next.get(new_stp, 0) | new_vals
-            dp = {}
-            seen = 0
-            for k in sorted(dp_next):
-                v = dp_next[k] & (~seen)
-                if v != 0:
-                    dp[k] = v
-                    seen |= v
-                    if seen == lim:
-                        break
-        res = 10000000
-        for k, v in dp.items():
-            if (v & (1 << sum)) != 0:
-                res = min(res, k)
-        if res == 10000000:
-            return -1
-        return res
+    def minCost(self, grid: list[list[int]], k: int) -> int:
+        DIRS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        m, n = len(grid), len(grid[0])
+        INF = 10**18
+        costs = [
+            [[[INF] * (k + 1) for _ in range(4)] for _ in range(n)] for _ in range(m)
+        ]
+        steps = [[[INF] * 4 for _ in range(n)] for _ in range(m)]
+
+        pq = []
+        for d in range(4):
+            costs[0][0][d][0] = grid[0][0]
+            heapq.heappush(pq, (grid[0][0], 0, 0, d, 0))
+
+        while pq:
+            c, x, y, d, t = heapq.heappop(pq)
+            if c != costs[x][y][d][t] or steps[x][y][d] <= t:
+                continue
+            steps[x][y][d] = t
+            if x == m - 1 and y == n - 1:
+                return c
+
+            for nd, (dx, dy) in enumerate(DIRS):
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n:
+                    nt = t + (nd != d)
+                    if nt <= k:
+                        nc = c + grid[nx][ny]
+                        if nc < costs[nx][ny][nd][nt]:
+                            costs[nx][ny][nd][nt] = nc
+                            heapq.heappush(pq, (nc, nx, ny, nd, nt))
+
+        return -1
 
